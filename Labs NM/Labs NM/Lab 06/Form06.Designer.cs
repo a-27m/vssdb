732f87a79a3e25a00e4d4ce6e0eb67b37be74945
Axell <Axell@bf672a44-3a04-1d4a-850b-c2a239875c8c>
2006-12-19 09:05:00 +0000

    git-svn-id: https://vaio/svn/vssdb@61 bf672a44-3a04-1d4a-850b-c2a239875c8c

--- Labs NM/Labs NM/Lab 06/Form06.Designer.cs
+++ Labs NM/Labs NM/Lab 06/Form06.Designer.cs
@@ -1,5 +1,5 @@
 namespace Lab_06 {
-	partial class Form1 {
+	partial class Form06 {
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
@@ -23,20 +23,48 @@ namespace Lab_06 {
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.button1 = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
-			// Form1
+			// richTextBox1
+			// 
+			this.richTextBox1.BackColor = System.Drawing.SystemColors.Control;
+			this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.richTextBox1.Location = new System.Drawing.Point(30, 21);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(184, 104);
+			this.richTextBox1.TabIndex = 0;
+			this.richTextBox1.Text = "Hello";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(298, 104);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form06
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(386, 269);
-			this.Name = "Form1";
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(418, 269);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.richTextBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Name = "Form06";
+			this.Text = "NML6v11(SP041s) \"Differential equations solution\"";
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.Button button1;
 	}
 }
 
---
+++ Labs NM/Labs NM/Lab 06/Form06.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Lab_06
+{
+	public partial class Form06 : Form
+	{
+		public Form06()
+		{
+			InitializeComponent();
+			richTextBox1.Rtf = @"";
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			for ( int i=0;i<10 ; i++)
+				;
+		}
+	}
+}
