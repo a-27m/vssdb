35824b7794d2983ec36f05e226dfc616d1efc95e
Axell <Axell@bf672a44-3a04-1d4a-850b-c2a239875c8c>
2007-02-07 20:07:22 +0000

    git-svn-id: https://vaio/svn/vssdb@91 bf672a44-3a04-1d4a-850b-c2a239875c8c

--- ОМиС/ОМиС/Лаб1/Form1.Designer.cs
+++ ОМиС/ОМиС/Лаб1/Form1.Designer.cs
@@ -28,30 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-			this.SuspendLayout();
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackColor = System.Drawing.Color.LightGray;
-			this.ClientSize = new System.Drawing.Size(467, 344);
-			this.DoubleBuffered = true;
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-			this.MaximizeBox = false;
-			this.Name = "Form1";
-			this.Opacity = 0.85;
-			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
-			this.Text = "ОМиС, лаб. 1, \"Моделирование измерений\"";
-			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-			this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
-			this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
-			this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
-			this.ResumeLayout(false);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.stripLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stripLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 0);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(467, 28);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // stripLabel1
+            // 
+            this.stripLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.stripLabel1.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.stripLabel1.Name = "stripLabel1";
+            this.stripLabel1.Size = new System.Drawing.Size(27, 23);
+            this.stripLabel1.Text = "—";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(467, 344);
+            this.Controls.Add(this.statusStrip1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Opacity = 0.85;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "ОМиС, лаб. 1, \"Моделирование измерений\"";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel stripLabel1;
 
 	}
 }
--- ОМиС/ОМиС/Лаб1/Form1.cs
+++ ОМиС/ОМиС/Лаб1/Form1.cs
@@ -9,97 +9,136 @@ using System.Windows.Forms;
 
 namespace Лаб1
 {
-	public partial class Form1 : Form
-	{
-		GraphicsPath grPath;
-		Timer timer;
-		double deltaTime=0;
+    public partial class Form1 : Form
+    {
+        GraphicsPath grPath;
+        DateTime datetimeStart;
+        double speed = -1;
 
-		public Form1()
-		{
-			InitializeComponent();
-			grPath = new GraphicsPath(FillMode.Winding);
-			timer = new Timer();
-			timer.Interval = 100;
-			timer.Tick += new EventHandler(timer_Tick);
-		}
 
-		void timer_Tick(object sender, EventArgs e)
-		{
-			if ( IsInAction )
-				deltaTime += timer.Interval;
-			else
-				deltaTime = 0;
-			Refresh();
-		}
+        public Form1()
+        {
+            InitializeComponent();
+            grPath = new GraphicsPath();
+            datetimeStart = new DateTime();
+        }
 
-		private void Form1_Paint(object sender, PaintEventArgs e)
-		{
-			e.Graphics.DrawPath(Pens.Black, grPath);
-			Font font = new Font(FontFamily.GenericSansSerif, 20f);			
-			e.Graphics.DrawString(
-				string.Format("{0:F2}",GetPathLength()/deltaTime),
-				font,
-				Brushes.Black,
-				new PointF(0,0));
-		}
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //PrintInfo(e.Graphics);
+        }
 
-		private int mx = 0, my = 0;
-		private bool IsInAction;
+        private void PrintInfo()
+        {
+            //g.SmoothingMode = SmoothingMode.AntiAlias;
+            ////e.Graphics.DrawPath(Pens.Black, grPath);
+            //Font font = new Font(FontFamily.GenericSansSerif, 16f);
 
-		private void Form1_MouseMove(object sender, MouseEventArgs e)
-		{
-			if ( !IsInAction )
-			{
-				grPath.Reset();
-				mx = e.X;
-				my = e.Y;
-				return;
-			}
+            //if (IsInAction)
+            //{
+            //    TimeSpan ts = DateTime.Now.Subtract(datetimeStart);
+            //    g.DrawString(
+            //        string.Format("{0:F2} точек; {1:F2} секунд.", GetPathLength(), ts.TotalSeconds),
+            //        font,
+            //        Brushes.Black,
+            //        new PointF(0, 0));
+            //}
+            //else
+            //{
+            //    g.DrawString(
+            //        string.Format("Зарегистрирована скорость: {0:F3} точек в секунду.", speed),
+            //        font,
+            //        Brushes.Black,
+            //        new PointF(0, 0));
+            //}
 
-			if ( grPath.PointCount < 1 )
-			{
-				grPath.StartFigure();
-				grPath.AddLine(e.X, e.Y, e.X + 1, e.Y + 1);
-			}
-			grPath.AddLine(mx, my, e.X, e.Y);
+            if (IsInAction)
+            {
+                TimeSpan ts = DateTime.Now.Subtract(datetimeStart);
+                stripLabel1.Text =
+                    string.Format("{0:F2} точек; {1:F2} секунд.", GetPathLength(), ts.TotalSeconds);
+            }
+            else
+            {
+                stripLabel1.Text =
+                    string.Format("Зарегистрирована скорость: {0:F3} точек в секунду.", speed);
+            }
 
-			mx = e.X;
-			my = e.Y;
+        }
 
-			Refresh();
-		}
+        private int mx = 0, my = 0;
+        private bool IsInAction;
 
-		private void Form1_MouseClick(object sender, MouseEventArgs e)
-		{
-			if ( e.Button == MouseButtons.Left )
-				timer.Enabled = (IsInAction = !IsInAction);
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!IsInAction)
+            {
+                grPath.Reset();
+                mx = e.X;
+                my = e.Y;
+                return;
+            }
+            else
+            {
+                if (grPath.PointCount < 1)
+                    grPath.StartFigure();
 
-			if ( e.Button == MouseButtons.Right )
-			{
-				IsInAction = false;
-				timer.Enabled = false;
-				deltaTime = 0;
-				grPath.Reset();
-				mx = e.X;
-				my = e.Y;
+                grPath.AddLine(mx, my, e.X, e.Y);
+                Graphics g = CreateGraphics();
+                g.SmoothingMode = SmoothingMode.AntiAlias;
+                g.DrawLine(Pens.Black, mx, my, e.X, e.Y);
+                PrintInfo();
 
-				Refresh();
-			}
-		}
+                //Size sz = new Size(Math.Abs(mx - e.X)+4, Math.Abs(my - e.Y)+4);
+                //Point pt = new Point((mx < e.X) ? mx-2 : e.X-2, (my < e.Y) ? my-2: e.Y-2);
+                //this.Invalidate(new Rectangle(pt, sz));
 
-		protected double GetPathLength()
-		{
-			double len = 0;
-			for ( int i = 1; i < grPath.PointCount; i++ )
-			{
-				PointF p1 = grPath.PathPoints[i-1];
-				PointF p2 = grPath.PathPoints[i];
-				len += Math.Sqrt(
-					( p2.X - p1.X ) * ( p2.X - p1.X ) +
-					( p2.Y - p1.Y ) * ( p2.Y - p1.Y ));
-			}
-			return len;
-		}
-	}
+                mx = e.X;
+                my = e.Y;
+            }
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            mx = e.X;
+            my = e.Y;
+
+
+            if (e.Button == MouseButtons.Left)
+            {
+                IsInAction = !IsInAction;
+                if (IsInAction)
+                {
+                    grPath.Reset();
+                    datetimeStart = DateTime.Now;
+                }
+                else
+                {
+                    speed = GetPathLength() / DateTime.Now.Subtract(datetimeStart).TotalSeconds;
+                    Refresh();
+                }
+
+            }
+
+            if (e.Button == MouseButtons.Right)
+            {
+                IsInAction = false;
+                Refresh();
+            }
+        }
+
+        protected double GetPathLength()
+        {
+            double len = 0;
+            for (int i = 1; i < grPath.PointCount; i++)
+            {
+                PointF p1 = grPath.PathPoints[i - 1];
+                PointF p2 = grPath.PathPoints[i];
+                len += Math.Sqrt(
+                    (p2.X - p1.X) * (p2.X - p1.X) +
+                    (p2.Y - p1.Y) * (p2.Y - p1.Y));
+            }
+            return len;
+        }
+    }
 }
