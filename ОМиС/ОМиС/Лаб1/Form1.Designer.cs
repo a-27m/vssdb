5d92972e3bbdb978bbb5cdc01188c66992cea2a7
Axell <Axell@bf672a44-3a04-1d4a-850b-c2a239875c8c>
2007-02-08 07:29:40 +0000

    git-svn-id: https://vaio/svn/vssdb@92 bf672a44-3a04-1d4a-850b-c2a239875c8c

--- ОМиС/ОМиС/Лаб1/Form1.Designer.cs
+++ ОМиС/ОМиС/Лаб1/Form1.Designer.cs
@@ -30,7 +30,10 @@
         {
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.stripLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.statusStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // statusStrip1
@@ -40,17 +43,53 @@
             this.stripLabel1});
             this.statusStrip1.Location = new System.Drawing.Point(0, 0);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(467, 28);
+            this.statusStrip1.Size = new System.Drawing.Size(467, 24);
             this.statusStrip1.TabIndex = 0;
             this.statusStrip1.Text = "statusStrip1";
             // 
             // stripLabel1
             // 
             this.stripLabel1.BackColor = System.Drawing.Color.Transparent;
-            this.stripLabel1.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.stripLabel1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.stripLabel1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.stripLabel1.Name = "stripLabel1";
-            this.stripLabel1.Size = new System.Drawing.Size(27, 23);
+            this.stripLabel1.Size = new System.Drawing.Size(421, 19);
+            this.stripLabel1.Spring = true;
             this.stripLabel1.Text = "—";
+            this.stripLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("Wingdings", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button1.Location = new System.Drawing.Point(383, 270);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(61, 27);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "N";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 67F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 33F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(467, 320);
+            this.tableLayoutPanel1.TabIndex = 2;
+            this.tableLayoutPanel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.tableLayoutPanel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             // 
             // Form1
             // 
@@ -58,8 +97,10 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightGray;
             this.ClientSize = new System.Drawing.Size(467, 344);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.statusStrip1);
             this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Opacity = 0.85;
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
@@ -70,6 +111,7 @@
             this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -79,6 +121,8 @@
 
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel stripLabel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
 
 	}
 }
--- ОМиС/ОМиС/Лаб1/Form1.cs
+++ ОМиС/ОМиС/Лаб1/Form1.cs
@@ -13,60 +13,79 @@ namespace Лаб1
     {
         GraphicsPath grPath;
         DateTime datetimeStart;
+        Timer timerForStrip;
+        Pen pen;
         double speed = -1;
-
+        double PathLength = 0;
 
         public Form1()
         {
             InitializeComponent();
+            //this.Opacity = 1;
             grPath = new GraphicsPath();
             datetimeStart = new DateTime();
+            timerForStrip = new Timer();
+            timerForStrip.Interval = 100;
+            timerForStrip.Tick += new EventHandler(timerForStrip_Tick);
+
+            pen = new Pen(Brushes.Black, 2f);
+            pen.StartCap = pen.EndCap = LineCap.RoundAnchor;
+        }
+
+        void timerForStrip_Tick(object sender, EventArgs e)
+        {
+            if (IsInAction)
+                PrintInfo(null);
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            e.Graphics.Clear(BackColor);
+
+            int grStepX = 20;
+            int grStepY = 20;
+
+            for (int x = 0; x < this.Width; x += grStepX)
+                e.Graphics.DrawLine(Pens.SteelBlue, x, 0, x, this.Height);
+
+            for (int y = 0; y < this.Height; y += grStepY)
+                e.Graphics.DrawLine(Pens.SteelBlue, 0, y, this.Width, y);
+
+             grStepX = 100;
+             grStepY = 100;
+
+            for (int x = 0; x < this.Width; x += grStepX)
+                e.Graphics.DrawLine(Pens.Black, x, 0, x, this.Height);
+
+            for (int y = 0; y < this.Height; y += grStepY)
+                e.Graphics.DrawLine(Pens.Black, 0, y, this.Width, y);
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            //PrintInfo(e.Graphics);
+            PrintInfo(e.Graphics);
         }
 
-        private void PrintInfo()
+        private void PrintInfo(Graphics g)
         {
-            //g.SmoothingMode = SmoothingMode.AntiAlias;
-            ////e.Graphics.DrawPath(Pens.Black, grPath);
-            //Font font = new Font(FontFamily.GenericSansSerif, 16f);
-
-            //if (IsInAction)
-            //{
-            //    TimeSpan ts = DateTime.Now.Subtract(datetimeStart);
-            //    g.DrawString(
-            //        string.Format("{0:F2} точек; {1:F2} секунд.", GetPathLength(), ts.TotalSeconds),
-            //        font,
-            //        Brushes.Black,
-            //        new PointF(0, 0));
-            //}
-            //else
-            //{
-            //    g.DrawString(
-            //        string.Format("Зарегистрирована скорость: {0:F3} точек в секунду.", speed),
-            //        font,
-            //        Brushes.Black,
-            //        new PointF(0, 0));
-            //}
-
             if (IsInAction)
             {
                 TimeSpan ts = DateTime.Now.Subtract(datetimeStart);
                 stripLabel1.Text =
-                    string.Format("{0:F2} точек; {1:F2} секунд.", GetPathLength(), ts.TotalSeconds);
+                    string.Format("{0:F2} точек; {1:F2} секунд. (~{2:F2} т/с)", PathLength, ts.TotalSeconds, PathLength / ts.TotalSeconds);
             }
             else
             {
                 stripLabel1.Text =
-                    string.Format("Зарегистрирована скорость: {0:F3} точек в секунду.", speed);
+                    string.Format("Зарегистрирована скорость: {1:F2}т/{2:F2}с = {0:F3} точек в секунду.", speed, PathLength, PathLength / speed);
+                g.SmoothingMode = SmoothingMode.HighQuality;
+                g.DrawPath(pen, grPath);
+                g.Dispose();
             }
-
         }
 
         private int mx = 0, my = 0;
+        DateTime mt;
         private bool IsInAction;
 
         private void Form1_MouseMove(object sender, MouseEventArgs e)
@@ -74,8 +93,10 @@ namespace Лаб1
             if (!IsInAction)
             {
                 grPath.Reset();
+                mt = DateTime.Now;
                 mx = e.X;
                 my = e.Y;
+
                 return;
             }
             else
@@ -83,16 +104,22 @@ namespace Лаб1
                 if (grPath.PointCount < 1)
                     grPath.StartFigure();
 
+                double deltaLen = Math.Sqrt((e.X - mx) * (e.X - mx) + (e.Y - my) * (e.Y - my));
+                PathLength += deltaLen;
+
+                PrintInfo(null);
+
                 grPath.AddLine(mx, my, e.X, e.Y);
-                Graphics g = CreateGraphics();
+
+                Graphics g = tableLayoutPanel1.CreateGraphics();
                 g.SmoothingMode = SmoothingMode.AntiAlias;
                 g.DrawLine(Pens.Black, mx, my, e.X, e.Y);
-                PrintInfo();
 
                 //Size sz = new Size(Math.Abs(mx - e.X)+4, Math.Abs(my - e.Y)+4);
                 //Point pt = new Point((mx < e.X) ? mx-2 : e.X-2, (my < e.Y) ? my-2: e.Y-2);
                 //this.Invalidate(new Rectangle(pt, sz));
 
+                mt = DateTime.Now;
                 mx = e.X;
                 my = e.Y;
             }
@@ -100,9 +127,9 @@ namespace Лаб1
 
         private void Form1_MouseClick(object sender, MouseEventArgs e)
         {
+            mt = DateTime.Now;
             mx = e.X;
             my = e.Y;
-
 
             if (e.Button == MouseButtons.Left)
             {
@@ -111,13 +138,15 @@ namespace Лаб1
                 {
                     grPath.Reset();
                     datetimeStart = DateTime.Now;
+                    PathLength = 0;
+                    Refresh();
                 }
                 else
                 {
-                    speed = GetPathLength() / DateTime.Now.Subtract(datetimeStart).TotalSeconds;
-                    Refresh();
+                    speed = PathLength / DateTime.Now.Subtract(datetimeStart).TotalSeconds;
+                    PrintInfo(null);
                 }
-
+                timerForStrip.Enabled = IsInAction;
             }
 
             if (e.Button == MouseButtons.Right)
@@ -127,18 +156,23 @@ namespace Лаб1
             }
         }
 
-        protected double GetPathLength()
+        private void button1_Click(object sender, EventArgs e)
         {
-            double len = 0;
-            for (int i = 1; i < grPath.PointCount; i++)
-            {
-                PointF p1 = grPath.PathPoints[i - 1];
-                PointF p2 = grPath.PathPoints[i];
-                len += Math.Sqrt(
-                    (p2.X - p1.X) * (p2.X - p1.X) +
-                    (p2.Y - p1.Y) * (p2.Y - p1.Y));
-            }
-            return len;
+            Application.Exit();
         }
+
+        //protected double GetPathLength()
+        //{
+        //    double len = 0;
+        //    for (int i = 1; i < grPath.PointCount; i++)
+        //    {
+        //        PointF p1 = grPath.PathPoints[i - 1];
+        //        PointF p2 = grPath.PathPoints[i];
+        //        len += Math.Sqrt(
+        //            (p2.X - p1.X) * (p2.X - p1.X) +
+        //            (p2.Y - p1.Y) * (p2.Y - p1.Y));
+        //    }
+        //    return len;
+        //}
     }
 }
