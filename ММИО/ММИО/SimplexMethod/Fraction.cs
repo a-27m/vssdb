33860cb7102b95c60b37751dff2214b104f98d47
Axell <Axell@bf672a44-3a04-1d4a-850b-c2a239875c8c>
2007-02-27 22:54:56 +0000

    git-svn-id: https://vaio/svn/vssdb@117 bf672a44-3a04-1d4a-850b-c2a239875c8c

--- ММИО/ММИО/SimplexMethod/Fraction.cs
+++ ММИО/ММИО/SimplexMethod/Fraction.cs
@@ -50,6 +50,8 @@ namespace Fractions
         long m_numerator;
         long m_denominator;
 
+        bool m_syraja = true;
+
         public Fraction(long integer, long numerator, long denominator)
         {
             if (denominator >= 0)
@@ -62,6 +64,7 @@ namespace Fractions
                 this.m_numerator = -numerator;
                 this.m_denominator = -denominator;
             }
+            m_syraja = true;
         }
 
         public Fraction(long numerator, long denominator)
@@ -76,6 +79,7 @@ namespace Fractions
                 this.m_numerator = -numerator;
                 this.m_denominator = -denominator;
             }
+            m_syraja = true;
         }
 
         public Decimal Value
@@ -97,6 +101,7 @@ namespace Fractions
                 m_denominator = (long)Math.Pow(10, digitsCount);
                 m_numerator = (long)decfrac;
                 m_integer = (long)(value - Math.Truncate(value));
+                this.Simplify();
             }
         }
 
@@ -138,6 +143,74 @@ namespace Fractions
             {
                 return m_integer;
             }
+        }
+
+        public virtual void Simplify()
+        {
+            long gcd = GCD(Math.Abs(m_numerator), m_denominator);
+            m_numerator /= gcd;
+            m_denominator /= gcd;
+            m_syraja = false;
+        }
+        public static Fraction Add(Fraction a, Fraction b)
+        {
+            long lcm = LCM(a.m_denominator, b.m_denominator);
+            long d_a = lcm / a.m_denominator;
+            long d_b = lcm / b.m_denominator;
+            Fraction sum = new Fraction(a.m_integer + b.m_integer, a.m_numerator * d_a + b.m_numerator * d_b, lcm);
+            sum.Simplify();
+            return sum;
+        }
+        public static Fraction Negate(Fraction a)
+        {
+            return new Fraction(a.m_integer, -a.m_numerator, a.m_denominator);
+        }
+        public static Fraction Substract(Fraction a, Fraction b)
+        {
+            long lcm = LCM(a.m_denominator, b.m_denominator);
+            long d_a = lcm / a.m_denominator;
+            long d_b = lcm / b.m_denominator;
+            Fraction sum = new Fraction(a.m_integer - b.m_integer, a.m_numerator * d_a - b.m_numerator * d_b, lcm);
+            sum.Simplify();
+            return sum;
+        }
+        public static Fraction Multiply(Fraction a, Fraction b)
+        {
+            Fraction mul = new Fraction(
+                (a.m_integer * a.m_denominator + a.m_numerator) *
+                (b.m_integer * b.m_denominator + b.m_numerator),
+                a.m_denominator * b.m_denominator);
+            mul.Simplify();
+            return mul;
+        }
+        public static Fraction Divide(Fraction a, Fraction b)
+        {
+            Fraction div = new Fraction(
+                (a.m_integer * a.m_denominator + a.m_numerator) * b.m_denominator,
+                (b.m_integer * b.m_denominator + b.m_numerator) * a.m_denominator);
+            div.Simplify();
+            return div;
+        }
+
+        public static long GCD(long a, long b)
+        {
+            // Алгоритм Евклида для поиска НОД
+            while (a != b)
+            {
+                if (a > b)
+                {
+                    a = a - b;
+                    continue;
+                }
+                if (b > a)
+                    b = b - a;
+            }
+            return a; // == b
+        }
+
+        public static long LCM(long a, long b)
+        {
+            return a * b / GCD(a, b);
         }
 
         public object Clone()
