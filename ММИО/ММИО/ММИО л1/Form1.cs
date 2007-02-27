04cd68b0e5ac45df1cefe8603bab41c5ee6a5fa0
Axell <Axell@bf672a44-3a04-1d4a-850b-c2a239875c8c>
2007-02-27 14:41:34 +0000

    git-svn-id: https://vaio/svn/vssdb@115 bf672a44-3a04-1d4a-850b-c2a239875c8c

--- ММИО/ММИО/ММИО л1/Form1.cs
+++ ММИО/ММИО/ММИО л1/Form1.cs
@@ -1,18 +1,72 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using DekartGraphic;
 
 namespace ММИО_л1
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
-    }
+	public partial class Form1 : Form
+	{
+		int n = 0, m = 0;
+
+		public int N
+		{
+			get { return n; }
+			set { n = value; numericUpDownN.Value = value; }
+		}
+
+		public int M
+		{
+			get { return m; }
+			set { m = value; numericUpDownM.Value = value; }
+		}
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+		{
+			dataGridView1.AutoResizeColumn(e.ColumnIndex);
+		}
+
+		private void dataGridView1_RowsAdded(object sender, DataGridViewRowsAddedEventArgs e)
+		{
+			this.M = dataGridView1.RowCount-1;
+		}
+
+		private void buttonOK_Click(object sender, EventArgs e)
+		{
+			N = (int)(numericUpDownN.Value);
+			M = (int)(numericUpDownM.Value);
+			GridMyResize(N, M);
+			dataGridView1.Select();
+		}
+
+		private void GridMyResize(int vars, int conds)
+		{
+			dataGridView1.Columns.Clear();
+			dataGridView1.Rows.Clear();
+			DataGridViewColumn col;
+
+			for ( int i = 0; i < vars; i++ )
+			{
+			col = new DataGridViewColumn(CoefCol0.CellTemplate);
+				col.HeaderText = "x"+(i+1).ToString();
+				dataGridView1.Columns.Add(col);
+			}
+
+			dataGridView1.Columns.Add(new DataGridViewColumn(SignCol.CellTemplate));
+
+			col = new DataGridViewColumn(CoefCol0.CellTemplate);
+			col.HeaderText = "b";
+			dataGridView1.Columns.Add(col);
+
+			dataGridView1.Rows.Add(conds + 1);
+
+			dataGridView1.AutoResizeColumns();
+		}
+	}
 }
--- ММИО/ММИО/ММИО л1/Properties/AssemblyInfo.cs
+++ ММИО/ММИО/ММИО л1/Properties/AssemblyInfo.cs
@@ -6,13 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ММИО л1")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ММИО л1")]
-[assembly: AssemblyCopyright("Copyright ©  2007")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyDescription("Лабораторные работы по курсу ММИО, ЗГИА©")]
+[assembly: AssemblyProduct("ММИО лабс")]
+[assembly: AssemblyCopyright("Alexey Mironenko (aka Axell) ©  2007")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
