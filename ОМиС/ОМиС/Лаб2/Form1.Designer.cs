90070b7fc81c450770173ee65a97afefc9e59915
Axell <Axell@bf672a44-3a04-1d4a-850b-c2a239875c8c>
2007-03-27 06:32:44 +0000

    git-svn-id: https://vaio/svn/vssdb@163 bf672a44-3a04-1d4a-850b-c2a239875c8c

--- ОМиС/ОМиС/Лаб2/Form1.Designer.cs
+++ ОМиС/ОМиС/Лаб2/Form1.Designer.cs
@@ -2,6 +2,26 @@
 {
     partial class Form1
     {
+        public class TextBoxMy : System.Windows.Forms.TextBox
+        {
+            private System.Windows.Forms.LinkLabel target;
+            public System.Windows.Forms.LinkLabel Target
+            {
+                get
+                {
+                    return target;
+                }
+                set
+                {
+                    if (value != null)
+                    {
+                        Location = value.Location + value.Size;
+                        target = value;
+                    }
+                }
+            }
+        }
+
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -28,12 +48,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.linkLabel1 = new Лаб2.LinkLabelMy();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Enabled = false;
+            this.trackBar1.Location = new System.Drawing.Point(260, 12);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(45, 163);
+            this.trackBar1.TabIndex = 0;
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Enabled = false;
+            this.trackBar2.Location = new System.Drawing.Point(311, 12);
+            this.trackBar2.Maximum = 100;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar2.Size = new System.Drawing.Size(45, 163);
+            this.trackBar2.TabIndex = 0;
+            this.trackBar2.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(12, 12);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(89, 13);
+            this.linkLabel1.TabIndex = 3;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Начало отсчета:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(639, 187);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Шкалы";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private LinkLabelMy linkLabel1;
     }
 }
 
--- ОМиС/ОМиС/Лаб2/Form1.cs
+++ ОМиС/ОМиС/Лаб2/Form1.cs
@@ -14,5 +14,123 @@ namespace Лаб2
         {
             InitializeComponent();
         }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+          //  linkLabel1.DefaultText;
+        }
+    }
+
+    public class FormPopup : Form
+    {
+        TextBox textBox = null;
+        GroupBox grBox = null;
+        Button button = null;
+
+        public FormPopup(LinkLabel linklabel)
+        {
+            grBox = new GroupBox();
+            textBox = new TextBox();
+            button = new Button();
+
+            grBox.SuspendLayout();
+
+            textBox.Name = "textBox";
+            textBox.Size = new Size(100, 50);
+            textBox.Location = new Point(8, 11);
+            textBox.Text = linklabel.Text;
+
+            button.Name = "button";
+            button.Size = new Size(textBox.Size.Height, textBox.Size.Height);
+            button.Font = new Font("Symbol", 6f);
+            button.Location = new Point(
+                textBox.Location.X + textBox.Size.Width + 4,
+                textBox.Location.Y);
+            button.TextAlign = ContentAlignment.TopCenter;
+            button.Text = "®";
+
+            grBox.Controls.Add(textBox);
+            grBox.Controls.Add(button);
+            grBox.Dock = DockStyle.Fill;
+
+            this.Controls.Add(textBox);
+            this.Controls.Add(button);
+            this.Controls.Add(grBox);
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Location = linklabel.Parent.PointToScreen(linklabel.Location) + linklabel.Size;
+            this.Size = textBox.Size + new Size(16 + 6 + button.Width, 16);
+            this.ShowInTaskbar = false;
+            this.StartPosition = FormStartPosition.Manual;
+
+            grBox.ResumeLayout(false);
+            grBox.PerformLayout();
+
+            textBox.Select();
+
+            textBox.KeyPress += new KeyPressEventHandler(textBox_KeyPress);
+            button.Click += new EventHandler(button_Click);
+            this.Deactivate += new EventHandler(FormPopup_Deactivate);
+        }
+
+        void textBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+                button_Click(sender, e);
+            if (e.KeyChar == (char)Keys.Escape)
+                FormPopup_Deactivate(sender, e);
+        }
+        void FormPopup_Deactivate(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+        void button_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            this.Text = textBox.Text;
+            this.Close();
+        }
+    }
+
+    public class LinkLabelMy : LinkLabel
+    {
+        FormPopup fp=null;
+
+        string m_value= "";
+       public  string Value
+        {
+            get
+            {
+                return m_value;
+            }
+            set
+            {
+                m_value = value;
+            }
+        }
+
+        protected override void OnLinkClicked(LinkLabelLinkClickedEventArgs e)
+        {
+        fp = new FormPopup(this);
+            fp.Show();
+            fp.FormClosed += new FormClosedEventHandler(fp_FormClosed);
+            base.OnLinkClicked(e);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            string str = Text;
+            Text += " " + Value;
+            base.OnPaint(e);
+            Text = str;
+        }
+
+        void fp_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.Value = fp.Text;
+            this.AutoSize = true;
+            this.Size = this.PreferredSize;
+        }
+
     }
 }
