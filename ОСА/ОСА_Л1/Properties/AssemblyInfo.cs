0e6177a6ea51018e698b19f1d704a87a812dadce
Axell <Axell@bf672a44-3a04-1d4a-850b-c2a239875c8c>
2007-04-19 12:27:58 +0000

    git-svn-id: https://vaio/svn/vssdb@188 bf672a44-3a04-1d4a-850b-c2a239875c8c

---
+++ ОСА/ОСА_Л1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ОСА_Л1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("POMM")]
+[assembly: AssemblyProduct("ОСА_Л1")]
+[assembly: AssemblyCopyright("Copyright © POMM 2007")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7e8dddcf-d3fa-4d10-96fb-4b9891413dda")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
