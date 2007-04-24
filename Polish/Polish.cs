66301f0c151afa653333d51d82c1016901f66d27
Axell <Axell@bf672a44-3a04-1d4a-850b-c2a239875c8c>
2007-04-24 23:31:08 +0000

    git-svn-id: https://vaio/svn/vssdb@201 bf672a44-3a04-1d4a-850b-c2a239875c8c

--- Polish/Polish.cs
+++ Polish/Polish.cs
@@ -1,10 +1,50 @@
 using System;
-using System.Collections.Generic;
 using System.Text;
+
+using automats;
 
 namespace Polish
 {
-    public class Class1
+    public class Polish
     {
+        public delegate double DoubleFunction(double x);
+
+        string prefix;
+
+        //static protected DoubleFunction[] functions = new DoubleFunction[] {
+        //    new DoubleFunction(Math.Sin),
+        //    new DoubleFunction(Math.Cos),      
+        //    new DoubleFunction(Math.Tan),      
+        //    new DoubleFunction(Math.),      
+        //    new DoubleFunction(Math),      
+        //    new DoubleFunction(Math),      
+        //    new DoubleFunction(Math),      
+        //    new DoubleFunction(Math),      
+        //    new DoubleFunction(Math),      
+        //    new DoubleFunction(Math),};
+
+        enum State
+        {
+            Initial,
+        }
+
+
+        public Polish(string infix)
+        {
+            double c;
+            State currState;
+
+            for (int i = 0; i < infix.Length; i++)
+            {
+                char s = infix[i];
+
+                switch (currState)
+                {
+                    case State.Initial:
+
+                        break;
+                }
+            }
+        }
     }
 }
