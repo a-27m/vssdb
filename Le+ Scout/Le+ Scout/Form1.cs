9eeaa57cbe6f3ca701fd568b01dd5346a3b9c90c
Axell <Axell@bf672a44-3a04-1d4a-850b-c2a239875c8c>
2008-01-29 11:32:32 +0000

    git-svn-id: https://vaio/svn/vssdb@232 bf672a44-3a04-1d4a-850b-c2a239875c8c

--- Le+ Scout/Le+ Scout/Form1.cs
+++ Le+ Scout/Le+ Scout/Form1.cs
@@ -63,6 +63,7 @@ namespace Le__Scout
 
         private void button1_Click(object sender, EventArgs e)
         {
+            qTableAdapter.
             SelectByCode(textBox1.Text, dataSet1.Tables["chetab"]);
 
             //MySqlCommand cmd = new MySqlCommand();
@@ -107,6 +108,8 @@ namespace Le__Scout
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            // TODO: This line of code loads data into the 'leplusDataSet.q' table. You can move, or remove it, as needed.
+            this.qTableAdapter.Fill(this.leplusDataSet.q);
             connectToolStripMenuItem_Click(sender, e);
         }
     }
