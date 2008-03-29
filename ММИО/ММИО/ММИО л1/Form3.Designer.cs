06ef057c1d1220cbf9ba11b779f1033666354e87
Axell <Axell@bf672a44-3a04-1d4a-850b-c2a239875c8c>
2008-03-29 18:05:18 +0000

    git-svn-id: https://vaio/svn/vssdb@273 bf672a44-3a04-1d4a-850b-c2a239875c8c

--- ММИО/ММИО/ММИО л1/Form3.Designer.cs
+++ ММИО/ММИО/ММИО л1/Form3.Designer.cs
@@ -43,12 +43,12 @@
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Location = new System.Drawing.Point(0, 24);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(572, 424);
+            this.dataGridView1.Size = new System.Drawing.Size(572, 400);
             this.dataGridView1.TabIndex = 0;
-            this.dataGridView1.Paint += new System.Windows.Forms.PaintEventHandler(this.dataGridView1_Paint);
             this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
+            this.dataGridView1.Paint += new System.Windows.Forms.PaintEventHandler(this.dataGridView1_Paint);
             // 
             // menuStrip1
             // 
