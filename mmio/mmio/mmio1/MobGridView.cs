291accec2625d04572e291d3a94ab266f686148b
Axell <Axell@bf672a44-3a04-1d4a-850b-c2a239875c8c>
2009-02-06 17:29:30 +0000

    git-svn-id: https://vaio/svn/vssdb@287 bf672a44-3a04-1d4a-850b-c2a239875c8c

---
+++ mmio/mmio/mmio1/MobGridView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace mmio1
+{
+    public partial class MobGridView : UserControl
+    {
+        public MobGridView()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+        }
+    }
+}
