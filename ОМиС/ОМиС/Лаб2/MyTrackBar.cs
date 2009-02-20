bdbe4901b38500ed4fb16cd09866e7346ccafb33
Axell <Axell@bf672a44-3a04-1d4a-850b-c2a239875c8c>
2009-02-20 07:29:06 +0000

    git-svn-id: https://vaio/svn/vssdb@310 bf672a44-3a04-1d4a-850b-c2a239875c8c

---
+++ ОМиС/ОМиС/Лаб2/MyTrackBar.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Лаб2
+{
+    public partial class MyTrackBar : TrackBar
+    {
+        public MyTrackBar()
+            : base()
+        {
+            InitializeComponent();
+        }
+
+        Form1.ScaleParams ps;
+
+        protected override void OnValueChanged(EventArgs e)
+        {
+            base.OnValueChanged(e);
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            //base.OnPaint(e);
+
+            e.Graphics.DrawString(ps.S0.ToString(),
+                new Font("Arial", FontHeight),
+                Brushes.Black,
+                10f, 10f);
+        }
+    }
+}
