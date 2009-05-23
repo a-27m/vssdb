9f5c713f9a28bc82eae327b31da9a67ce08cafb1
Axell <Axell@bf672a44-3a04-1d4a-850b-c2a239875c8c>
2009-05-23 17:09:00 +0000

    git-svn-id: https://vaio/svn/vssdb@345 bf672a44-3a04-1d4a-850b-c2a239875c8c

---
+++ DB/NWTradersLab/NWTradersLab/Insert Alexey M.sql
@@ -0,0 +1,8 @@
+/****** Script for Inserting myslf into employes table ******/
+insert into Employees 
+	( [LastName] ,[FirstName] ,[Title] ,[TitleOfCourtesy], [BirthDate], [HireDate],
+	[Address] ,[City] ,[Region] ,[PostalCode] ,[Country] ,[HomePhone] ,[Extension],
+	[Photo] ,[Notes] ,[ReportsTo] ,[PhotoPath] )
+values ('Mironenko' ,'Alexey' ,'Project manager' ,'Prof.' ,'1988-04-30' ,'2009-01-06',
+	'Charivnaya 109a/110' ,'Zaporozhye' ,null ,'69074' ,'Ukraine' ,'(38061) 213-5783' ,'123',
+	null ,'Just genius' ,null ,null )
