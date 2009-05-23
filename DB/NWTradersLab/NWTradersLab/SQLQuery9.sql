698994a01691db56ebe4c00ecebf1528fcd16b42
Axell <Axell@bf672a44-3a04-1d4a-850b-c2a239875c8c>
2009-05-23 17:12:10 +0000

    git-svn-id: https://vaio/svn/vssdb@346 bf672a44-3a04-1d4a-850b-c2a239875c8c

--- DB/NWTradersLab/NWTradersLab/SQLQuery9.sql
+++ DB/NWTradersLab/NWTradersLab/SQLQuery9.sql
@@ -4,13 +4,19 @@
 */
 
 /*
-select COUNT(*) as [HowManyOrder], Employees.FirstName, Employees.LastName
-from Orders inner join Employees on Orders.EmployeeID = Employees.EmployeeID
-group by Employees.EmployeeID, Employees.FirstName, Employees.LastName
-order by 1 desc
+  'distinct' in the Count tells to count distinct NONNULL rows, so the worse seller gets 0
+becouse of nulls in joined columns, added by left join)
 */
-
-select count(distinct OrderID) as [HowManyOrder], Employees.FirstName, Employees.LastName
-from Employees left join Orders on Employees.EmployeeID = Orders.EmployeeID
-group by Employees.EmployeeID, Employees.FirstName, Employees.LastName
-order by 1 desc
+select
+	count(distinct OrderID) as [HowManyOrder],
+	Employees.FirstName,
+	Employees.LastName
+from
+	Employees left join Orders
+		on Employees.EmployeeID = Orders.EmployeeID
+group by
+	Employees.EmployeeID,
+	Employees.FirstName,
+	Employees.LastName
+order by
+	1 desc;
