56a8fd69cb4e163abc2ceef94acc94c1f4de5052
Axell <Axell@bf672a44-3a04-1d4a-850b-c2a239875c8c>
2009-05-23 22:34:24 +0000

    git-svn-id: https://vaio/svn/vssdb@348 bf672a44-3a04-1d4a-850b-c2a239875c8c

--- DB/NWTradersLab/NWTradersLab/SQLQuery4.sql
+++ DB/NWTradersLab/NWTradersLab/SQLQuery4.sql
@@ -4,3 +4,35 @@
     (Использовать квантор существования).
     См. п.1.
 */
+
+-- no quantors
+select distinct 
+	Products.ProductID,
+	Products.ProductName,
+	[Order Details].Discount
+from
+	Products inner join [Order Details] on Products.ProductID = [Order Details].ProductID
+where 
+	Discount > 0
+	and ProductName = 'Maxilaku'
+order by
+	Discount desc;
+
+-- with quantor
+select distinct 
+	p.ProductID,
+	p.ProductName,
+	o.Discount
+from
+	Products p inner join [Order Details] o on p.ProductID = o.ProductID
+where exists (
+	select *
+	from [Order Details] t
+	where t.Discount > 0
+	  and t.ProductID = p.ProductID
+	)
+ and o.Discount > 0
+ and p.ProductName = 'Maxilaku'
+order by
+	o.Discount desc;
+	
