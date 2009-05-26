c54f3073b2a8c82b15e382f2414a7e03ac42a632
Axell <Axell@bf672a44-3a04-1d4a-850b-c2a239875c8c>
2009-05-26 22:38:36 +0000

    git-svn-id: https://vaio/svn/vssdb@351 bf672a44-3a04-1d4a-850b-c2a239875c8c

--- AlgTheory/ComplexRoots/Program.cs
+++ AlgTheory/ComplexRoots/Program.cs
@@ -14,7 +14,7 @@ namespace ComplexRoots
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormMain());
         }
     }
 }
