a5fb404fc9069c41ec5e50264e300e57281aa719
Axell <Axell@bf672a44-3a04-1d4a-850b-c2a239875c8c>
2009-06-04 14:08:50 +0000

    git-svn-id: https://vaio/svn/vssdb@359 bf672a44-3a04-1d4a-850b-c2a239875c8c

---
+++ AlgTheory/Lab 6 - Own vect and num/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Lab_6___Own_vect_and_num
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label label1;
+            this.dgvA = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dgvT = new System.Windows.Forms.DataGridView();
+            this.dgvX = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvA)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvT)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvX)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(24, 19);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(84, 13);
+            label1.TabIndex = 2;
+            label1.Text = "Размерность n";
+            // 
+            // dgvA
+            // 
+            this.dgvA.AllowUserToAddRows = false;
+            this.dgvA.AllowUserToDeleteRows = false;
+            this.dgvA.AllowUserToResizeRows = false;
+            this.dgvA.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.dgvA.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvA.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvA.Location = new System.Drawing.Point(315, 3);
+            this.dgvA.Name = "dgvA";
+            this.dgvA.Size = new System.Drawing.Size(307, 244);
+            this.dgvA.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.dgvA, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dgvT, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.dgvX, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 250F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(625, 550);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // dgvT
+            // 
+            this.dgvT.AllowUserToAddRows = false;
+            this.dgvT.AllowUserToDeleteRows = false;
+            this.dgvT.AllowUserToResizeRows = false;
+            this.dgvT.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.dgvT.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvT.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvT.Location = new System.Drawing.Point(315, 253);
+            this.dgvT.Name = "dgvT";
+            this.dgvT.Size = new System.Drawing.Size(307, 294);
+            this.dgvT.TabIndex = 1;
+            // 
+            // dgvX
+            // 
+            this.dgvX.AllowUserToAddRows = false;
+            this.dgvX.AllowUserToDeleteRows = false;
+            this.dgvX.AllowUserToResizeRows = false;
+            this.dgvX.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.dgvX.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvX.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvX.Location = new System.Drawing.Point(3, 253);
+            this.dgvX.Name = "dgvX";
+            this.dgvX.Size = new System.Drawing.Size(306, 294);
+            this.dgvX.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(label1);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(306, 244);
+            this.panel1.TabIndex = 3;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(27, 45);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(72, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Применить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(27, 146);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Рассчет";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 550);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Лаб 6 - Собственные числа и вектора";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvA)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvT)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvX)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvA;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView dgvT;
+        private System.Windows.Forms.DataGridView dgvX;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ AlgTheory/Lab 6 - Own vect and num/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Lab_6___Own_vect_and_num
+{
+    public partial class Form1 : Form
+    {
+        int n;
+        double[,] A, T, U;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            n = (int)numericUpDown1.Value;
+            SetSize(n);
+        }
+
+        private void SetSize(int n)
+        {
+            dgvA.Rows.Clear();
+            dgvA.Columns.Clear();
+
+            for (int i = 0; i < n; i++)
+            {
+                dgvA.Columns.Add("c" + (i + 1).ToString(), (i + 1).ToString());
+                dgvX.Columns.Add("c" + (i + 1).ToString(), (i + 1).ToString());
+                dgvT.Columns.Add("c" + (i + 1).ToString(), (i + 1).ToString());
+            }
+
+            dgvA.Rows.Add(n);
+        }
+
+        public void ReadA()
+        {
+            A = new double[n, n];
+
+            try
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        A[i, j] = double.Parse(dgvA[j, i].Value.ToString());
+                    }
+                }
+            }
+            catch (FormatException) { MessageBox.Show("Неверный формат числа"); }
+        }
+
+        public void PrintA()
+        {
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        dgvA[j, i].Value = A[i, j].ToString("F4");
+                    }
+                }
+        }
+
+        void Find()
+        {
+            T = new double[n, n];
+
+            PrepareT(0, 1);
+            U = T;
+
+            while (CheckZero() == false)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = i+1; j < n; j++)
+                    {
+                        PrepareT(i, j);
+                        Print(dgvT, T, true);
+
+                        A = MultMatrixReverse(T, A);
+                        A = MultMatrix(A, T);
+
+                        U = MultMatrix(U, T);
+                    }
+                }
+            }
+        }
+
+        private double[,] MultMatrix(double[,] A, double[,] B)
+        {
+            double[,] r = new double[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    r[i, j] += A[i, j] * B[j, i];
+                }
+            }
+
+            return r;
+        }
+
+        private double[,] MultMatrixReverse(double[,] A, double[,] TransB)
+        {
+            double[,] r = new double[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    r[i, j] += A[i, j] * TransB[i, j];
+                }
+            }
+
+            return r;
+        }
+
+        private void Print(DataGridView dgv, double[,] A, bool Add)
+        {
+            int offset = 0;
+            if (Add)
+            {
+                dgv.Rows.Add(n + 1);
+                offset = dgv.Rows.Count - A.GetLength(0);
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    dgv[j, i+ offset].Value = A[i, j].ToString("F4");
+                }
+            }
+        }
+
+
+        private void PrepareT(int I, int J)
+        {
+            T = new double[n,n];
+
+            double alpha = 0.5*Math.Atan(2*A[I,J] / (A[I,I] - A[J,J]));
+
+            for (int i = 0; i < n; i++) T[i,i] = 1;
+
+            T[I,I] = Math.Cos(alpha);
+            T[I,J] = Math.Sin(alpha);
+            T[J,I] = -Math.Sin(alpha);
+            T[J,J] = Math.Cos(alpha);
+        }
+
+        private bool CheckZero()
+        {
+            double sD = 0, sS = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == j) sD += A[i, j];
+                    else sS += A[i, j];
+                }
+            }
+
+            return sS < 0.001;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ReadA();
+            Find();
+            Print(dgvA, A, false);
+            Print(dgvX, U, true);
+        }
+
+        
+    }
+}
