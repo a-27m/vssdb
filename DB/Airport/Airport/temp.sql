773f76a44388711f4691e3698a930937724df383
Axell <Axell@bf672a44-3a04-1d4a-850b-c2a239875c8c>
2009-09-16 04:06:08 +0000

    git-svn-id: https://vaio/svn/vssdb@382 bf672a44-3a04-1d4a-850b-c2a239875c8c

--- DB/Airport/Airport/temp.sql
+++ DB/Airport/Airport/temp.sql
@@ -1,4 +1,9 @@
 use Airport;
 
-select *
-from Person;
+SELECT     Flight.IDFlight, Flight.ShortTitle, PlannedFlight.Departure, PlannedFlight.Price, PlannedRoute.Price AS Expr1, AirRoute.ShortTitle AS Expr2, 
+                      Ticket.IDTicket
+FROM         PlannedFlight INNER JOIN
+                      Flight ON PlannedFlight.IDFlight = Flight.IDFlight INNER JOIN
+                      PlannedRoute ON PlannedFlight.IDPlannedRoute = PlannedRoute.IDPlannedRoute INNER JOIN
+                      AirRoute ON PlannedRoute.IDAirRoute = AirRoute.IDRoute INNER JOIN
+                      Ticket ON PlannedRoute.IDPlannedRoute = Ticket.IDPlannedRoute
