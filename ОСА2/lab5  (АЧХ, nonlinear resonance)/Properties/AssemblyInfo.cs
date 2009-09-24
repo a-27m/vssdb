1b4bdd133a65f00b02a2efa0b94a0ba61d99cd92
Axell <Axell@bf672a44-3a04-1d4a-850b-c2a239875c8c>
2009-09-24 14:15:26 +0000

    git-svn-id: https://vaio/svn/vssdb@387 bf672a44-3a04-1d4a-850b-c2a239875c8c

--- ОСА2/lab5  (АЧХ, nonlinear resonance)/Properties/AssemblyInfo.cs
+++ ОСА2/lab5  (АЧХ, nonlinear resonance)/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("lab5  (АЧХ, nonlinear resonance)")]
+[assembly: AssemblyTitle("lab5")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
