2c77b902b08bdbe812d9bb001668a400a9628bb4
Axell <Axell@bf672a44-3a04-1d4a-850b-c2a239875c8c>
2009-10-07 15:30:46 +0000

    git-svn-id: https://vaio/svn/vssdb@403 bf672a44-3a04-1d4a-850b-c2a239875c8c

--- DB/Airport/Airport/Queries.sql
+++ DB/Airport/Airport/Queries.sql
@@ -1,8 +1,8 @@
 use Airport;
-
+GO
 /*
 1.	Одержати список і загальну кількість
-	1) всіх робітників аеpопоpту
+	1) всіх робітників аеpопоpту --- view Employee
 	2) керівників відділів
 	3) робітників зазначеного відділу
 	
@@ -14,14 +14,71 @@ use Airport;
 */
 
 -- все работники
+if exists (select name from Airport.sys.views where name = 'Employee') drop view Employee;
+GO
+
+create view Employee
+as
 select
 	*
 from
-	Person p
-where exists	
+	Person
 	 inner join 
 		(
-			select p.IDPerson as persid, p.Salary as salary, p.IDTeam from Pilot p
+			select p.IDPerson as persid, p.Salary as salary, p.IDTeam, p.IDDepartment, 'Пилот' as JobTitle from Pilot p
 			union
-			select p.IDPerson as persid, p.Salary as salary, p.IDTeam from Worker p
+			select p.IDPerson as persid, p.Salary as salary, p.IDTeam, p.IDDepartment, 'Рабочий' as JobTitle from Worker p
 		) empl on Person.IDPerson = empl.persid
+
+GO
+
+if exists (select name from Airport.sys.views where name = 'Boss') drop view Boss;
+GO
+
+create view Boss
+as
+select
+	'Бригада' as BossType, Team.IDTeam as ID, Person.*
+from
+	Team inner join Person on Team.Boss = Person.IDPerson
+union
+select
+	'Отдел' as BossType, Department.IDDepartment as ID, Person.*
+from
+	Department inner join Person on Department.Boss = Person.IDPerson
+
+GO
+
+if object_id (N'GetDepartmentEmployees', N'IF') is not null
+	drop function GetDepartmentEmployees;
+GO
+create function GetDepartmentEmployees (@DeptID int)
+returns table
+as
+return
+(
+	select * from Employee where IDDepartment = @DeptID
+);
+GO
+
+-- a) по стажу
+select
+	*, DateDiff(month, e.Hired, GetDate()) as Stage
+from
+	Employee e
+order by Stage desc
+
+-- b) sex
+select
+	*
+from
+	Employee e
+order by e.Sex desc
+
+-- c) age
+select
+	*, DateDiff(year, e.Birth, GetDate()) as Age
+from
+	Employee e
+order by Age desc
+
