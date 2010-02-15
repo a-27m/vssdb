e8b12fc3aa4d027bd3c8541ef2a715ca11e3b14d
Axell <Axell@bf672a44-3a04-1d4a-850b-c2a239875c8c>
2010-02-15 21:31:40 +0000

    git-svn-id: https://vaio/svn/vssdb@426 bf672a44-3a04-1d4a-850b-c2a239875c8c

---
+++ CG/cglr1/cglr1/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace cglr1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonRotate = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(658, 456);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonRotate);
+            this.panel1.Controls.Add(this.buttonClear);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(194, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(48, 66);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 0;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(203, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(452, 450);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // buttonRotate
+            // 
+            this.buttonRotate.Location = new System.Drawing.Point(48, 111);
+            this.buttonRotate.Name = "buttonRotate";
+            this.buttonRotate.Size = new System.Drawing.Size(75, 23);
+            this.buttonRotate.TabIndex = 1;
+            this.buttonRotate.Text = "Rotate";
+            this.buttonRotate.UseVisualStyleBackColor = true;
+            this.buttonRotate.Click += new System.EventHandler(this.buttonRotate_Click);
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 456);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Разложение в растр отрезков (КГ-ЛР1)";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonRotate;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ CG/cglr1/cglr1/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace cglr1
+{
+    public partial class Form1 : Form
+    {
+        List<Point> pts;
+        Random rnd;
+
+        public Form1()
+        {
+            InitializeComponent();
+            pts = new List<Point>();
+            rnd = new Random(DateTime.Now.Millisecond);
+        }
+
+        float cellSize = 3f;
+        Brush pixelBrush = Brushes.Black;
+
+        private void Plot(Graphics g, int x, int y)
+        {
+            g.FillRectangle(pixelBrush, x * cellSize + 1, y * cellSize + 1, cellSize, cellSize);
+        }
+
+        private void Plot(Graphics g, int x, int y, Color c)
+        {
+            (pixelBrush as SolidBrush).Color = c;
+            g.FillRectangle(pixelBrush, x * cellSize + 1, y * cellSize + 1, cellSize, cellSize);
+        }
+
+        Color ptc(float p)
+        {
+            int val = (int)(255f * p);
+            if (val < 0) return Color.Red;
+            if (val > 255) return Color.Blue;
+            return Color.FromArgb(val, val, val);
+        }
+
+        private void Line(Graphics g, int x1, int y1, int x2, int y2)
+        {
+            if (x1 == x2 && y1 == y2)
+            {
+                Plot(g, x1, y1);
+                return;
+            }
+
+            int x, y;
+            x = x1;
+            y = y1;
+            int Δx, Δy;
+            Δx = x2 - x1;
+            Δy = y2 - y1;
+            float ΔyΔx = (float)Δy / (float)Δx;
+            float e;
+
+            int dx = Math.Sign(Δx);
+            int dy = Math.Sign(Δy);
+
+            if (x1 == x2)
+            {
+                for (int i = y1; i <= y2; i += dy)
+                    Plot(g, x1, i);
+                return;
+            }
+
+            if (y1 == y2)
+            {
+                for (int i = x1; i < x2; i += dx)
+                    Plot(g, i, y1);
+                return;
+            }
+
+
+            if (ΔyΔx > 0)
+            {
+                e = ΔyΔx - 0.5f;
+                float e2 = e / 2f;
+
+                for (int i = 0; i < Math.Abs(Δx); i++)
+                {
+                    this.Plot(g, x, y);//, ptc(Math.Abs(e - e2)/e2));
+                    e2 = e / 2f;
+
+                    while (e >= 0)
+                    {
+                        y = y + dy;
+                        this.Plot(g, x, y);//, ptc(Math.Abs(e - e2) / e2));
+                        e = e - 1f;
+                    }
+                    x = x + dx;
+                    e = e + ΔyΔx;
+                }
+            }
+            else
+            {
+                e = -1f/ΔyΔx - 0.5f;
+
+                for (int i = 0; i < Math.Abs(Δy); i++)
+                {
+                    this.Plot(g, x, y);
+                    while (e >= 0)
+                    {
+                        x = x + dx;
+                        e = e - 1f;
+                        this.Plot(g, x, y);
+                    }
+                    y = y + dy;
+                    e = e - 1f/ΔyΔx;
+                }
+            }
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            pictureBox1.CreateGraphics().Clear(Color.White);
+            pts.Clear();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (pts == null) return;
+            if (pts.Count < 2) return;
+
+            Point prev = pts[0];
+            foreach (Point p in pts)
+            {
+                this.Line(e.Graphics, prev.X, prev.Y, p.X, p.Y);
+                prev = p;
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            pts.Add(new Point(
+                (int)(e.X / cellSize),
+                (int)(e.Y / cellSize)
+                ));
+            pictureBox1.Refresh();
+        }
+
+        private void buttonRotate_Click(object sender, EventArgs e)
+        {
+            //buttonClear_Click(sender, e);
+
+            timer.Enabled = !timer.Enabled;
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            //pts.Clear();
+
+            pts.Add(new Point(
+                rnd.Next((int)(pictureBox1.Width/cellSize)),
+                rnd.Next((int)(pictureBox1.Height/cellSize))));
+            pts.Add(new Point(
+                rnd.Next((int)(pictureBox1.Width / cellSize)),
+                rnd.Next((int)(pictureBox1.Height / cellSize))));
+
+            pictureBox1.Refresh();
+        }
+    }
+}
