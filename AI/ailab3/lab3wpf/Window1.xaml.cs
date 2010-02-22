ecdd7b69ddbb07aee8017c521d1d1dab911a8982
Axell <Axell@bf672a44-3a04-1d4a-850b-c2a239875c8c>
2010-02-22 06:15:20 +0000

    git-svn-id: https://vaio/svn/vssdb@424 bf672a44-3a04-1d4a-850b-c2a239875c8c

--- AI/ailab3/lab3wpf/Window1.xaml.cs
+++ AI/ailab3/lab3wpf/Window1.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using logic15;
 
 namespace lab3wpf
 {
@@ -19,37 +20,195 @@ namespace lab3wpf
     /// </summary>
     public partial class Window1 : Window
     {
-		
+		Board board;
+        Button[,] pieces;
+
         public Window1()
         {
             InitializeComponent();
+            but1.Visibility = Visibility.Hidden;
         }
 
         private void textBox1_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
         	// TODO: Add event handler implementation here.
-			textBox1.Text += 
-			textBox1.Visibility.ToString();
-			textBox1.Visibility = System.Windows.Visibility.Hidden;
+			//textBox1.Text += 
+			//textBox1.Visibility.ToString();
+            //textBox1.Visibility = System.Windows.Visibility.Hidden;
         }
 
-        private void Button_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {	
-			
-		}
+        protected void GenerateMesh(int rows, int cols)
+        {
+            gridField.RowDefinitions.Clear();
+            gridField.ColumnDefinitions.Clear();
+
+            RowDefinition rd;
+            ColumnDefinition cd;
+
+            for (int i = 0; i < rows; i++)
+            {
+                rd = new RowDefinition();
+                rd.Height = new GridLength(1f / rows, GridUnitType.Star);
+                gridField.RowDefinitions.Add(rd);
+            }
+
+            for (int j = 0; j < cols; j++)
+            {
+                cd = new ColumnDefinition();
+                cd.Width = new GridLength(1f / cols, GridUnitType.Star);
+                gridField.ColumnDefinitions.Add(cd);
+            }
+        }
 
         private void but1_Click(object sender, System.Windows.RoutedEventArgs e)
         {
 			int r, c;
+            Button bt = (sender as Button);
 			
-			c = Grid.GetColumn(but1);
-			r = Grid.GetRow(but1);
+			c = Grid.GetColumn(bt);
+            r = Grid.GetRow(bt);
+            int OldEmptyCol = board.EmptyCol;
+            int OldEmptyRow = board.EmptyRow;
 
+
+            if (board[r, c].IsEmpty) return;
+
+            if (!board.Turn(r, c))
+            {
+                // no such turn
+                bt.Background = Brushes.Red;
+                return;
+            }
+
+            // log
 			textBox1.Text += c.ToString() + ";" + r.ToString() + Environment.NewLine;
-			//gridField.Children.Remove(but1);
-			Grid.SetColumn(but1, c+1);
-			Grid.SetRow(but1, r+1);
-			//gridField.Children.Add(but1);
+
+            // update buttons
+            Grid.SetColumn(pieces[r, c], OldEmptyCol);
+            Grid.SetRow(pieces[r, c], OldEmptyRow);
+            Swap(r, c, OldEmptyRow, OldEmptyCol);
+        }
+
+        protected void Swap(int i1, int j1, int i2, int j2)
+        {
+            Button t = pieces[i1, j1];
+
+            pieces[i1, j1] = pieces[i2, j2];
+            pieces[i2, j2] = t;
+        }
+
+        private void but1_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+        	// TODO: Add event handler implementation here.
+        }
+
+        private void but1_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+        	// TODO: Add event handler implementation here.
+        }
+
+        private void load_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            board = Board.Load("4x4.csv");
+            board.SetAsEtalon();
+
+            GenerateMesh(board.Rows, board.Columns);
+
+            //int no = 1;
+            pieces = new Button[board.Rows, board.Columns];
+
+            for (int i = 0; i < pieces.GetLength(0); i++)
+            {
+                for (int j = 0; j < pieces.GetLength(1); j++)
+                {
+                    if (board[i, j].IsEmpty) { continue; }
+
+                    pieces[i, j] = new Button();
+                    pieces[i, j].Click += but1_Click;
+                 //  pieces[i, j].MouseEnter += Triggers).First());//.MouseEnter; OnMouseEnter1;
+                    pieces[i, j].Effect = but1.Effect;
+                    pieces[i, j].Margin = but1.Margin;
+                    pieces[i, j].Style = but1.Style;
+                    pieces[i, j].FontFamily = but1.FontFamily;
+                    pieces[i, j].FontSize = but1.FontSize;
+                    pieces[i, j].FontStyle = but1.FontStyle;
+                    pieces[i, j].FontWeight = but1.FontWeight;
+                    pieces[i, j].Content = board[i, j].Value;//no++.ToString(); //(i * board.Columns + j + 1).ToString();
+
+                    Grid.SetColumn(pieces[i, j], j);
+                    Grid.SetRow(pieces[i, j], i);
+                    gridField.Children.Add(pieces[i, j]);
+                }
+            }
+        }
+
+        private void mix_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            textBox1.AppendText(string.Join("; ",
+                board.EnumerateValidTurns().AsQueryable<Direction>().Select(d => d.ToString()).ToArray()));
+
+            return;
+
+			Random rnd = new Random(DateTime.Now.Millisecond);
+
+            for (int i = 0; i < 10; i++)
+            {
+                int oldi, oldj;
+                oldi = board.EmptyRow;
+                oldj = board.EmptyCol;
+
+                IEnumerable<Direction> turns;
+                turns = board.EnumerateValidTurns();
+                board.Turn(turns.ToArray()[rnd.Next(turns.Count())]);
+
+                // update buttons
+                Swap(oldi, oldj, board.EmptyRow, board.EmptyCol);
+            }
+
+            for (int i = 0; i < pieces.GetLength(0); i++)
+            {
+                for (int j = 0; j < pieces.GetLength(1); j++)
+                {
+                    if (pieces[i, j] == null) continue;
+                    Grid.SetColumn(pieces[i, j], j);
+                    Grid.SetRow(pieces[i, j], i);
+                }
+            }
+        }
+
+        private void solve_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            board.HeuristicsSearch();
+            List<Board> list = board.GeneratePath();
+
+            board = list[0];
+
+            gridField.Children.Clear();
+            pieces = new Button[board.Rows, board.Columns];
+
+            for (int i = 0; i < pieces.GetLength(0); i++)
+            {
+                for (int j = 0; j < pieces.GetLength(1); j++)
+                {
+                    if (board[i, j].IsEmpty) { continue; }
+
+                    pieces[i, j] = new Button();
+                    pieces[i, j].Click += but1_Click;
+                    //  pieces[i, j].MouseEnter += Triggers).First());//.MouseEnter; OnMouseEnter1;
+                    pieces[i, j].Effect = but1.Effect;
+                    pieces[i, j].Margin = but1.Margin;
+                    pieces[i, j].Style = but1.Style;
+                    pieces[i, j].FontFamily = but1.FontFamily;
+                    pieces[i, j].FontSize = but1.FontSize;
+                    pieces[i, j].FontStyle = but1.FontStyle;
+                    pieces[i, j].FontWeight = but1.FontWeight;
+                    pieces[i, j].Content = board[i, j].Value;//no++.ToString(); //(i * board.Columns + j + 1).ToString();
+
+                    Grid.SetColumn(pieces[i, j], j);
+                    Grid.SetRow(pieces[i, j], i);
+                    gridField.Children.Add(pieces[i, j]);
+                }
+            }
         }
     }
 }
--- AI/ailab3/logic15/logic15.cs
+++ AI/ailab3/logic15/logic15.cs
@@ -12,7 +12,11 @@ namespace logic15
         /// <summary>
         /// neihbours
         /// </summary>
-        Cell nUp, nDown, nLeft, nRight;
+        //Cell nUp, nDown, nLeft, nRight;
+
+        object val;
+
+        public object Value { get { return val; } set { val = value; } }
 
         public Cell()
         {
@@ -36,14 +40,28 @@ namespace logic15
         }
     }
 
-    public enum Direction { Up, Down, Left, Right }
+    public enum Direction { Up=0, Down, Left, Right }
 
     // TODO Validation: Only 1 empty cell should be
     public class Board
     {
         Cell[,] map;
 
+        internal float f, g, h;
+        internal Board previous;
+        internal Direction turn;
+
         int iEmpty, jEmpty;
+
+        public int EmptyCol
+        {
+            get { return jEmpty; }
+        }
+
+        public int EmptyRow
+        {
+            get { return iEmpty; }
+        }
 
         int[] positions;
         int N;
@@ -62,17 +80,32 @@ namespace logic15
             set { map[i, j] = value; }
         }
 
-		public IEnumerable<Direction> EnumerateValidTurns()
-		{
+        public int Rows { get { return map.GetLength(0); } }
+        public int Columns { get { return map.GetLength(1); } }
+
+        public IEnumerable<Direction> EnumerateValidTurns()
+        {
             // eval which cells are around ijEmpty
             // than check if they can take the empty place
 
-			return null;
-		}
-	
+            if (iEmpty + 1 < map.GetLength(0))
+                yield return Direction.Down;
+
+            if (jEmpty + 1 < map.GetLength(1))
+                yield return Direction.Right;
+
+            if (iEmpty - 1 >= 0)
+                yield return Direction.Up;
+
+            if (jEmpty - 1 >= 0)
+                yield return Direction.Left;
+
+            yield break;
+        }
+
         public static Board Load(string FileName)
         {
-            Board board;            
+            Board board;
 
             StreamReader f = null;
             try
@@ -98,24 +131,18 @@ namespace logic15
 
                     for (int j = 0; j < m; j++)
                     {
-                        switch (strs[j][0])
-                        { 
-                            case '.':
-                                board.map[i, j] = null;
-                                break;
-                            case '1':
-                                board.map[i, j] = new Cell(false);
-                                break;
-                            case '0':
-                                board.map[i, j] = new Cell(true);
-                                break;
-                            default:
-                                Debug.WriteLine(string.Format("Wrong input character '{0}' in file '{1}'",
-                                    strs[i][0], FileName));
-
-                                board.map[i, j] = null;
-                                break;
+                        if (strs[j].Length == 0)
+                        {
+                            board.map[i, j] = new Cell(true);
+                            board.iEmpty = i;
+                            board.jEmpty = j;
                         }
+                        else
+                        {
+                            board.map[i, j] = new Cell(false);
+                        }
+
+                        board.map[i, j].Value = strs[j];
                     }
 
                     i++;
@@ -137,34 +164,55 @@ namespace logic15
 
         public bool Turn(Direction dir)
         {
-            int x, y;
+            int x = iEmpty, y = jEmpty;
 
             switch (dir)
             {
-                case Direction.Up:
+                case Direction.Left:
                     x = iEmpty;
                     y = jEmpty - 1;
                     break;
-                case Direction.Down:
+                case Direction.Right:
                     x = iEmpty;
                     y = jEmpty + 1;
                     break;
-                case Direction.Left:
+                case Direction.Up:
                     x = iEmpty - 1;
                     y = jEmpty;
                     break;
-                case Direction.Right:
+                case Direction.Down:
                     x = iEmpty + 1;
                     y = jEmpty;
                     break;
             }
 
-            if (1 == 1) return false;// check if turn is valid;
+            if (1 != 1) return false;// check if turn is valid;
 
             Swap(x, y, iEmpty, jEmpty);
 
             iEmpty = x;
             jEmpty = y;
+
+            turn = dir;
+            return true;
+        }
+
+        public bool Turn(int r, int c)
+        {
+            if (r - iEmpty > 1) return false;
+            if (c - jEmpty > 1) return false;
+
+
+            if (Math.Abs(r - iEmpty) * Math.Abs(c - jEmpty) != 0) return false;
+
+            Swap(r, c, iEmpty, jEmpty);
+
+            // set this.turn
+
+            iEmpty = r;
+            jEmpty = c;
+
+            return true;
         }
 
         protected void Swap(int i1, int j1, int i2, int j2)
@@ -173,6 +221,167 @@ namespace logic15
 
             map[i1, j1] = map[i2, j2];
             map[i2, j2] = t;
+        }
+
+        List<Board> lOpen, lClosed;
+        bool traversed;
+        int q = 0;
+        public Board initState;
+        public Board etalonState;
+
+        public bool HeuristicsSearch()
+        {
+            //init all
+            lOpen = new List<Board>();
+            lClosed = new List<Board>();
+            this.previous = null;
+            g = 0;
+            initState = this;
+
+            //1. Поместить все узлы из множества So в список OPEN.
+            lOpen.Add(initState);
+
+            traversed = false;
+            while (!traversed)
+            {
+                //Log(string.Format("Open: {0}, closed: {1}.", lOpen.Count, lClosed.Count));
+
+                if (lOpen.Count == 0)
+                {
+                    traversed = true;
+                    break;
+                }
+
+                // search [open] for pos and q
+                int pos = 0;
+                f = float.MaxValue;
+                foreach (Board bd in lOpen)
+                {
+                    h = MeasureNotAtPlace(bd, etalonState);
+                    if (h + bd.g < f)
+                    {
+                        f = h+bd.g;
+                        q = pos;
+                    }
+
+                    pos++;
+                }
+
+                if (MeasureNotAtPlace(lOpen[q], etalonState) == 0)
+                {
+                    return true;
+                }
+
+                Board t = null;
+               //q = 0; // в ширину
+                //4. Раскрыть вершину n и все порождённые вершины поместить в список OPEN настроив указатели к вершине n
+                foreach (Direction dir in lOpen[q].EnumerateValidTurns())
+                {
+                    if (dir == ReverseTurn(lOpen[q].turn)) continue;
+
+                    t = (Board)lOpen[q].MemberwiseClone();
+                    t.map = (Cell[,])lOpen[q].map.Clone();
+                    t.g = lOpen[q].g + 1;
+                    t.Turn(dir);
+                    t.previous = lOpen[q];
+
+                //5. Если порожденная вершина целевая, т.е. принадлежит Sq то выдать решение с помощью указателей, иначе перейти к шагу №2.
+                if (MeasureNotAtPlace(t, etalonState) == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    lOpen.Add(t);
+                    //lOpen.Insert(p, t);
+                    //if (q >= p) q += p;
+                }
+                }
+
+                lClosed.Add(lOpen[q]);
+                lOpen.RemoveAt(q);
+            }
+
+            //Log(string.Format("Best path EVR: {0:#.##}", bestPathLen));
+            return false;
+        }
+
+        private Direction ReverseTurn(Direction direction)
+        {
+            switch (direction)
+            {
+                case Direction.Up:
+                    return Direction.Down;
+                    break;
+                case Direction.Down:
+                    return Direction.Up;
+                    break;
+                case Direction.Left:
+                    return Direction.Right;
+                    break;
+                case Direction.Right:
+                    return Direction.Left;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+                    break;
+            }
+        }
+
+        private float MeasureNotAtPlace(Board bd, Board etalonState)
+        {
+            int count = 0;
+            for (int i = 0; i < bd.map.GetLength(0); i++)
+            {
+                for (int j = 0; j < bd.map.GetLength(1); j++)
+                {
+                    if (bd[i, j].Value != etalonState[i, j].Value) count++;
+                }
+            }
+            return count;
+        }
+
+
+        public void SetAsEtalon()
+        {
+            etalonState = (Board)this.MemberwiseClone();
+            etalonState.map = (Cell[,])this.map.Clone();
+        }
+
+        public override string ToString()
+        {
+            string res = "";
+
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    if (map[i, j].IsEmpty) res += "    ";
+                    else res += " " + map[i, j].Value + " ";
+                }
+                res += Environment.NewLine;
+            }
+
+            return res;
+        }
+
+        List<Board> path = null;
+        public List<Board> GeneratePath()
+        {
+            if (path == null)
+                path = new List<Board>();
+            else
+                path.Clear();
+
+            path.Add(lOpen[q]);
+            Board curr = lOpen[q];
+            while (curr != null)
+            {
+                path.Add(curr);
+                curr = curr.previous;
+            }
+
+            return path;
         }
     }
 }
