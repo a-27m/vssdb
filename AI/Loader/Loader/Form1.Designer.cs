0b690f392c9d4658ac4d87451eeb2fd26b8611c8
Axell <Axell@bf672a44-3a04-1d4a-850b-c2a239875c8c>
2010-03-03 21:39:58 +0000

    git-svn-id: https://vaio/svn/vssdb@447 bf672a44-3a04-1d4a-850b-c2a239875c8c

--- AI/Loader/Loader/Form1.Designer.cs
+++ AI/Loader/Loader/Form1.Designer.cs
@@ -28,12 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonHeuristics = new System.Windows.Forms.Button();
+            this.buttonPerebor = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 63);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(340, 357);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(9, 18);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 1;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonHeuristics
+            // 
+            this.buttonHeuristics.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonHeuristics.Location = new System.Drawing.Point(186, 18);
+            this.buttonHeuristics.Margin = new System.Windows.Forms.Padding(3, 3, 10, 3);
+            this.buttonHeuristics.Name = "buttonHeuristics";
+            this.buttonHeuristics.Size = new System.Drawing.Size(75, 23);
+            this.buttonHeuristics.TabIndex = 2;
+            this.buttonHeuristics.Text = "Heuristics";
+            this.buttonHeuristics.UseVisualStyleBackColor = true;
+            this.buttonHeuristics.Click += new System.EventHandler(this.buttonHeuristics_Click);
+            // 
+            // buttonPerebor
+            // 
+            this.buttonPerebor.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonPerebor.Location = new System.Drawing.Point(105, 18);
+            this.buttonPerebor.Margin = new System.Windows.Forms.Padding(3, 3, 10, 3);
+            this.buttonPerebor.Name = "buttonPerebor";
+            this.buttonPerebor.Size = new System.Drawing.Size(75, 23);
+            this.buttonPerebor.TabIndex = 3;
+            this.buttonPerebor.Text = "Traverse";
+            this.buttonPerebor.UseVisualStyleBackColor = true;
+            this.buttonPerebor.Click += new System.EventHandler(this.buttonPerebor_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.HideSelection = false;
+            this.textBox1.Location = new System.Drawing.Point(349, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.tableLayoutPanel1.SetRowSpan(this.textBox1, 2);
+            this.textBox1.Size = new System.Drawing.Size(182, 417);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 188F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(534, 423);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonClear);
+            this.panel1.Controls.Add(this.buttonPerebor);
+            this.panel1.Controls.Add(this.buttonHeuristics);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(340, 54);
+            this.panel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(534, 423);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Loader";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonHeuristics;
+        private System.Windows.Forms.Button buttonPerebor;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- AI/Loader/Loader/Form1.cs
+++ AI/Loader/Loader/Form1.cs
@@ -1,19 +1,607 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.Drawing.Drawing2D;
 using System.Windows.Forms;
 
 namespace Loader
 {
     public partial class Form1 : Form
     {
+        class Disposition
+        {
+            private int? robotAt = null;
+            public int? RobotAt
+            {
+                get { return robotAt; }
+            }
+
+            private float currentCost = 0f;
+            public float TotalCost
+            {
+                get { return currentCost; }
+            }
+
+            private int[] boxAt = null;
+
+            private Disposition parent = null;
+            public Disposition Parent
+            {
+                get { return parent; }
+                set { parent = value; }
+            }
+
+            public delegate float CostEvaluator(int A, int B);
+
+            private CostEvaluator fullCost;
+
+            public Disposition(int? RobotAt, int BoxesCount, CostEvaluator FullCost)
+            {
+                boxAt = new int[BoxesCount];
+                for (int i = 0; i < BoxesCount; boxAt[i] = i++) ;
+
+                robotAt = RobotAt;
+
+                fullCost = FullCost;
+            }
+
+            public Disposition(Disposition disposition)
+            {
+                boxAt = new int[disposition.boxAt.Length];
+                for (int i = 0; i < boxAt.Length; i++)
+                    boxAt[i] = disposition.boxAt[i];
+
+                robotAt = disposition.robotAt;
+                fullCost = disposition.fullCost;
+                parent = disposition;
+                currentCost = disposition.currentCost;
+            }
+
+            public float GoTo(int pos)
+            {
+                float cost = fullCost(robotAt ?? -1, pos) / 2f;
+
+                currentCost += cost;
+                robotAt = pos;
+                return cost;
+            }
+
+            /// <summary>
+            /// Оnly the first box at iFrom will be moved
+            /// </summary>
+            public float Drag(int iFrom, int iTo)
+            {
+                if (robotAt != iFrom)
+                    throw new InvalidOperationException("Robot stands not at 'From' box");
+
+                for (int i = 0; i < boxAt.Length; i++)
+                    if (boxAt[i] == iFrom)
+                    {
+                        boxAt[i] = iTo;
+                        robotAt = iTo;
+                        currentCost += fullCost(iFrom, iTo);
+                        return fullCost(iFrom, iTo);
+                    }
+
+                throw new ArgumentOutOfRangeException("No boxes at 'From' location");
+            }
+
+            public float DragTo(int iTo)
+            {
+                return Drag(robotAt ?? -1, iTo);
+            }
+
+            public bool IsCollected
+            {
+                get
+                {
+                    int n = boxAt.Length;
+
+                    if (n < 2) return true;
+
+                    for (int i = 1; i < n; i++)
+                        if (boxAt[i - 1] != boxAt[i])
+                            return false;
+
+                    return true;
+                }
+            }
+
+            public int FindUncollected(int CollectionPointIndex)
+            {
+                for (int i = 0; i < boxAt.Length; i++)
+                {
+                    if (boxAt[i] != CollectionPointIndex)
+                        return i;
+                }
+
+                return -1;
+            }
+        }
+
+        List<Point> boxes;
+        List<Point> lines;
+        List<float> weights;
+        Point robot;
+        public float Tarif = 1f;
+
+        int d = 8;
+        Font fontLen;
+
         public Form1()
         {
             InitializeComponent();
+
+            boxes = new List<Point>();
+
+            robot.X = pictureBox1.Width >> 1;
+            robot.Y = pictureBox1.Height * 3 / 4;
+
+            fontLen = new Font("Arial", 10f);
+        }
+
+        #region I/O
+        private void DrawBox(Graphics g, Point at)
+        {
+            Pen penStroke = Pens.Black;
+            Brush brFill = new SolidBrush(Color.Firebrick);
+            int d2 = d << 1;
+            int d4 = d2 << 1;
+            int d_2 = d >> 1;
+
+            Rectangle[] recs = new Rectangle[3];
+            // Yellow
+            recs[0] = new Rectangle(at.X - d2, at.Y - d2, d4, d4);
+            // Green
+            recs[1] = new Rectangle(at.X - d, at.Y - d2, d2, d4);
+            // Blue
+            recs[2] = new Rectangle(at.X - d, at.Y - d, d2, d2);
+
+            g.FillRectangles(brFill, recs);
+            g.DrawRectangles(penStroke, recs);
+
+            g.DrawLine(penStroke, at.X - d, at.Y + d_2, at.X + d_2, at.Y - d);
+            g.DrawLine(penStroke, at.X - d_2, at.Y + d, at.X + d, at.Y - d_2);
+        }
+
+        private void DrawBot(Graphics g, Point at)
+        {
+            Pen penStroke = Pens.Black;
+            Brush brFill = new SolidBrush(Color.Silver);
+
+            Rectangle rect = new Rectangle(at.X - d * 3, at.Y - d * 3, d * 6, d * 6);
+            //g.FillRectangle(brFill, rect);
+            //g.DrawRectangle(penStroke, rect);
+            g.FillEllipse(brFill, rect);
+            g.DrawEllipse(penStroke, rect);
+        }
+
+        private void DrawRoads(Graphics g)
+        {
+            Random rnd = new Random();
+
+            if (lines != null)
+                if (lines.Count > 1)
+                {
+                    Point prev = lines[0];
+                    int iw = 0;
+                    foreach(Point curr in lines)
+                    {
+                        if (prev == curr)
+                        {
+                            iw++;
+                            continue;
+                        }
+
+                        g.DrawLine(Pens.Black, prev, curr);
+
+                        string caption = weights[iw].ToString("F1");
+                            //FullCost(prev, curr).ToString("F1");
+
+                        float t = 0.25f;// +0.333f * ((iw / 2) % 2); // (float)rnd.NextDouble();
+
+                        RectangleF rect = new RectangleF(
+                            prev.X + t*(curr.X - prev.X),
+                            prev.Y + t*(curr.Y - prev.Y),
+                            1,
+                            1);
+
+                        SizeF measure = g.MeasureString(
+                            caption,
+                            fontLen
+                            );
+
+                        rect.Inflate(measure.Width/2f, measure.Height/2f);
+
+                        g.FillRectangle(Brushes.YellowGreen, rect);
+                        g.DrawRectangle(Pens.Black, rect.Left, rect.Top, rect.Width, rect.Height);
+
+                        g.DrawString(
+                            caption,
+                            fontLen,
+                            Brushes.Black,
+                            rect.Left,
+                            rect.Top
+                            );
+
+                        prev = curr;
+                        iw++;
+                    }
+                }
+        }
+
+        private bool IsInside(Point ptCenter, Point ptClick, int width)
+        {
+            bool inside = true;
+
+            if (
+                (Math.Abs(ptClick.X - ptCenter.X) > width) ||
+                (Math.Abs(ptClick.Y - ptCenter.Y) > width)
+                ) inside = false;
+
+            return inside;
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Clear(pictureBox1.BackColor);
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            DrawGrid(e.Graphics, 100);
+
+            DrawRoads(e.Graphics);
+
+            foreach (Point p in boxes)
+                DrawBox(e.Graphics, p);
+
+            if (cursorOn == 2)
+                DrawBot(e.Graphics, ptCursor);
+            else
+                DrawBot(e.Graphics, robot);
+
+            if (cursorOn == 1)
+                DrawBox(e.Graphics, ptCursor);
+        }
+
+        private void DrawGrid(Graphics g, int step)
+        {
+            int w = pictureBox1.Width;
+            int h = pictureBox1.Height;
+
+            for (int x = 0; x < w; x+=step)
+                g.DrawLine(Pens.LightGray, x, 0, x, h);
+
+            for (int y = 0; y < h; y += step)
+                g.DrawLine(Pens.LightGray, 0, y, w, y);
+        }
+
+        Point ptCursor;
+        int cursorOn = 0;
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            ptCursor.X = e.X;
+            ptCursor.Y = e.Y;
+
+            if (cursorOn != 0)
+                pictureBox1.Refresh();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Point ptClick = new Point(e.X, e.Y);
+
+            // search if box is already there
+            for (int i = 0; i < boxes.Count; i++)
+            {
+                if (IsInside(boxes[i], ptClick, d * 2))
+                {
+                    // start move selected
+                    boxes.RemoveAt(i);
+                    cursorOn = 1;
+                }
+            }
+
+            if (IsInside(robot, ptClick, d * 3))
+            {
+                cursorOn = 2;
+            }
+
+            pictureBox1.Refresh();
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            Point ptClick = new Point(e.X, e.Y);
+
+            if (cursorOn == 2)
+            {
+                if (pictureBox1.ClientRectangle.Contains(ptClick))
+                {
+                    robot = ptClick;
+                }
+            }
+            else
+            {
+                if (!IsInside(robot, ptClick, d * 3))
+                {
+                    if (pictureBox1.ClientRectangle.Contains(ptClick))
+                        boxes.Add(ptClick);
+                }
+            }
+
+            cursorOn = 0;
+            pictureBox1.Refresh();
+        }
+        
+        #endregion
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            boxes.Clear();
+            lines.Clear();
+
+            pictureBox1.Refresh();
+        }
+
+        private void buttonPerebor_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "Traverse:";
+
+            List<Disposition> open, closed;
+
+            open = new List<Disposition>();
+            closed = new List<Disposition>();
+
+            Disposition initState =
+                new Disposition(null, boxes.Count, FullCost);
+
+            float minCost = float.MaxValue;
+            //int minCostFrom = -1, minCostTo = -1;
+            Disposition minCostDispos = null;
+
+            // where to collect
+            for (int i_where = 0; i_where < boxes.Count; i_where++)
+            {
+                open.Clear();
+
+                // from which to start
+                for (int i_from = 0; i_from < boxes.Count; i_from++)
+                {
+                    //if (i_from == i_where) continue;
+
+                    Disposition t = new Disposition(initState);
+                    t.GoTo(i_from);
+                    open.Add(t);
+                }
+
+                while (open.Count > 1)
+                {
+                    int q = 0;
+                    Disposition t = open[q];
+
+                    while (true)
+                    {
+                        int u = t.FindUncollected(i_where);
+                        if (u == -1) break;
+
+                        t = new Disposition(t);
+                        t.GoTo(u);
+                        closed.Add(t);
+
+                        t = new Disposition(t);
+                        t.DragTo(i_where);
+                        closed.Add(t);
+                    }
+
+                    if (minCost > t.TotalCost)
+                    {
+                        minCost = t.TotalCost;
+                        minCostDispos = t;
+                    }
+
+                    closed.Add(open[q]);
+                    open.RemoveAt(q);
+                }
+
+                if (minCostDispos == null)
+                    textBox1.Text += Environment.NewLine + "No solution";
+                else
+                    textBox1.Text += Environment.NewLine +
+                        minCostDispos.TotalCost.ToString("F2");
+
+            }
+
+            if (minCostDispos == null)
+            {
+                textBox1.Text += Environment.NewLine +
+                    "No solution" +
+                    Environment.NewLine;
+
+                return;
+            }
+
+            textBox1.Text += Environment.NewLine +
+                minCostDispos.TotalCost.ToString("F2") +
+                Environment.NewLine;
+
+            CreateRoads(minCostDispos);
+
+            pictureBox1.Refresh();
+        }
+
+        private void buttonHeuristics_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "Heuristics:";
+            if (boxes.Count < 1)
+            {
+                textBox1.Text += Environment.NewLine +
+                    "No boxes - no job" +
+                    Environment.NewLine;
+
+                return;
+            }
+
+            List<Disposition> open, closed;
+
+            open = new List<Disposition>();
+            closed = new List<Disposition>();
+
+            Disposition initState =
+                new Disposition(null, boxes.Count, FullCost);
+
+            float minCost = float.MaxValue;
+            Disposition minCostDispos = null;
+
+            // where to collect
+
+            int sumX = 0, sumY = 0;
+
+            for (int i = 0; i < boxes.Count; i++) 
+            { 
+                sumX += boxes[i].X;
+                sumY += boxes[i].Y;
+            }
+            
+            Point mid = new Point(sumX / boxes.Count, sumY / boxes.Count);
+
+            float minFC = float.MaxValue;
+            int i_where = -1;
+            for (int i = 0; i < boxes.Count; i++)
+            {
+                float fc = FullCost(mid, boxes[i]);
+                if (fc < minFC)
+                {
+                    minFC = fc;
+                    i_where = i;
+                }
+            }
+
+            {
+                open.Clear();
+
+                // from which to start
+                float nearLength = float.MaxValue;
+                int i_from = -1;
+                for (int i = 0; i < boxes.Count; i++)
+                {
+                    float fc = FullCost(robot, boxes[i]);
+                    if (fc < nearLength)
+                    {
+                        nearLength = fc;
+                        i_from = i;
+                    }
+                }
+
+                //for (int i_from = 0; i_from < boxes.Count; i_from++)
+                {
+                    Disposition t = new Disposition(initState);
+                    t.GoTo(i_from);
+                    open.Add(t);
+                }
+
+                while (open.Count > 0)
+                {
+                    int q = 0;
+                    Disposition t = open[q];
+
+                    while (true)
+                    {
+                        int u = t.FindUncollected(i_where);
+                        if (u == -1) break;
+
+                        t = new Disposition(t);
+                        t.GoTo(u);
+                        closed.Add(t);
+
+                        t = new Disposition(t);
+                        t.DragTo(i_where);
+                        closed.Add(t);
+                    }
+
+                    if (minCost > t.TotalCost)
+                    {
+                        minCost = t.TotalCost;
+                        minCostDispos = t;
+                    }
+
+                    closed.Add(open[q]);
+                    open.RemoveAt(q);
+                }
+
+                textBox1.Text +=  Environment.NewLine +
+                    minCostDispos.TotalCost.ToString("F2");
+
+            }
+
+            if (minCostDispos == null)
+            {
+                textBox1.Text += Environment.NewLine +
+                    "No solution" +
+                    Environment.NewLine;
+
+                return;
+            }
+
+            textBox1.Text += Environment.NewLine + 
+                minCostDispos.TotalCost.ToString("F2") + 
+                Environment.NewLine;
+
+            CreateRoads(minCostDispos);
+
+            pictureBox1.Refresh();
+        }
+
+        private void CreateRoads(Disposition minCostDispos)
+        {
+            lines = new List<Point>();
+            weights = new List<float>();
+            Disposition curr = minCostDispos;
+            Disposition prev = curr;
+            while (curr.Parent != null)
+            {
+                lines.Add(boxes[curr.RobotAt ?? 0]);
+                weights.Add(prev.TotalCost - curr.TotalCost);
+                prev = curr;
+                curr = curr.Parent;
+            }
+
+            lines.Add(robot);
+            weights.Add(
+                0.5f * FullCost(boxes[prev.RobotAt ?? -1], robot)
+                );
+        }
+
+        public float FullCost(int A, int B)
+        {
+            int dx, dy;
+
+            if (A == -1)
+            {
+                dx = boxes[B].X - robot.X;
+                dy = boxes[B].Y - robot.Y;
+            }
+            else
+            {
+                dx = boxes[B].X - boxes[A].X;
+                dy = boxes[B].Y - boxes[A].Y;
+            }
+
+            return (float)Math.Sqrt(dx * dx + dy * dy) * Tarif;
+        }
+        public float FullCost(Point A, Point B)
+        {
+            int dx, dy;
+
+            dx = B.X - A.X;
+            dy = B.Y - A.Y;
+        
+            return (float)Math.Sqrt(dx * dx + dy * dy) * Tarif;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.SelectionStart = textBox1.Text.Length;
+            textBox1.ScrollToCaret();
         }
     }
 }
