4d39e07ea4b8fa571c28c0160ce201b2544294f2
Axell <Axell@bf672a44-3a04-1d4a-850b-c2a239875c8c>
2010-03-11 21:55:12 +0000

    git-svn-id: https://vaio/svn/vssdb@455 bf672a44-3a04-1d4a-850b-c2a239875c8c

--- МАПР/mapr3/mapr3/Form1.Designer.cs
+++ МАПР/mapr3/mapr3/Form1.Designer.cs
@@ -29,11 +29,299 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Panel panel1;
+            System.Windows.Forms.Label label1;
+            System.Windows.Forms.Label label2;
+            System.Windows.Forms.Label label3;
+            System.Windows.Forms.Label label4;
+            System.Windows.Forms.Label label5;
+            System.Windows.Forms.Label label6;
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dgv1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxC1 = new System.Windows.Forms.TextBox();
+            this.textBoxC2 = new System.Windows.Forms.TextBox();
+            this.textBoxC3 = new System.Windows.Forms.TextBox();
+            this.textBoxN1 = new System.Windows.Forms.TextBox();
+            this.textBoxN2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBoxN = new System.Windows.Forms.TextBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            panel1 = new System.Windows.Forms.Panel();
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            label4 = new System.Windows.Forms.Label();
+            label5 = new System.Windows.Forms.Label();
+            label6 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 187F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dgv1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 108F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(678, 470);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(this.button3);
+            panel1.Controls.Add(this.button2);
+            panel1.Controls.Add(this.button1);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(this.textBoxN);
+            panel1.Controls.Add(this.textBoxN2);
+            panel1.Controls.Add(this.textBoxN1);
+            panel1.Controls.Add(this.textBoxC3);
+            panel1.Controls.Add(this.textBoxC2);
+            panel1.Controls.Add(this.textBoxC1);
+            panel1.Controls.Add(label6);
+            panel1.Controls.Add(label5);
+            panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            panel1.Location = new System.Drawing.Point(3, 3);
+            panel1.Name = "panel1";
+            this.tableLayoutPanel1.SetRowSpan(panel1, 2);
+            panel1.Size = new System.Drawing.Size(181, 464);
+            panel1.TabIndex = 0;
+            // 
+            // dgv1
+            // 
+            this.dgv1.AllowUserToAddRows = false;
+            this.dgv1.AllowUserToDeleteRows = false;
+            this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgv1.Location = new System.Drawing.Point(190, 3);
+            this.dgv1.Name = "dgv1";
+            this.dgv1.ReadOnly = true;
+            this.dgv1.Size = new System.Drawing.Size(485, 356);
+            this.dgv1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(190, 365);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(485, 102);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBoxC1
+            // 
+            this.textBoxC1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.errorProvider1.SetError(this.textBoxC1, "Введите целое число");
+            this.textBoxC1.Location = new System.Drawing.Point(101, 22);
+            this.textBoxC1.Name = "textBoxC1";
+            this.textBoxC1.Size = new System.Drawing.Size(37, 20);
+            this.textBoxC1.TabIndex = 0;
+            // 
+            // textBoxC2
+            // 
+            this.textBoxC2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.errorProvider1.SetError(this.textBoxC2, "Введите целое число");
+            this.textBoxC2.Location = new System.Drawing.Point(101, 49);
+            this.textBoxC2.Name = "textBoxC2";
+            this.textBoxC2.Size = new System.Drawing.Size(37, 20);
+            this.textBoxC2.TabIndex = 1;
+            // 
+            // textBoxC3
+            // 
+            this.textBoxC3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.errorProvider1.SetError(this.textBoxC3, "Введите целое число");
+            this.textBoxC3.Location = new System.Drawing.Point(101, 76);
+            this.textBoxC3.Name = "textBoxC3";
+            this.textBoxC3.Size = new System.Drawing.Size(37, 20);
+            this.textBoxC3.TabIndex = 2;
+            // 
+            // textBoxN1
+            // 
+            this.textBoxN1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.errorProvider1.SetError(this.textBoxN1, "Введите целое число");
+            this.textBoxN1.Location = new System.Drawing.Point(101, 119);
+            this.textBoxN1.Name = "textBoxN1";
+            this.textBoxN1.Size = new System.Drawing.Size(37, 20);
+            this.textBoxN1.TabIndex = 1;
+            // 
+            // textBoxN2
+            // 
+            this.textBoxN2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.errorProvider1.SetError(this.textBoxN2, "Введите целое число");
+            this.textBoxN2.Location = new System.Drawing.Point(101, 146);
+            this.textBoxN2.Name = "textBoxN2";
+            this.textBoxN2.Size = new System.Drawing.Size(37, 20);
+            this.textBoxN2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(11, 25);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(62, 13);
+            label1.TabIndex = 3;
+            label1.Text = "C1 (экспл.)";
+            // 
+            // label2
+            // 
+            label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(11, 52);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(65, 13);
+            label2.TabIndex = 3;
+            label2.Text = "C2 (проезд)";
+            // 
+            // label3
+            // 
+            label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(11, 79);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(62, 13);
+            label3.TabIndex = 3;
+            label3.Text = "C3 (штраф)";
+            // 
+            // label4
+            // 
+            label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            label4.AutoSize = true;
+            label4.Location = new System.Drawing.Point(11, 122);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(71, 13);
+            label4.TabIndex = 3;
+            label4.Text = "n1 (избыток)";
+            // 
+            // label5
+            // 
+            label5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            label5.AutoSize = true;
+            label5.Location = new System.Drawing.Point(9, 149);
+            label5.Name = "label5";
+            label5.Size = new System.Drawing.Size(86, 13);
+            label5.TabIndex = 3;
+            label5.Text = "n2 (недостаток)";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(28, 359);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(28, 388);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button3.Location = new System.Drawing.Point(28, 418);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            label6.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            label6.AutoSize = true;
+            label6.Location = new System.Drawing.Point(11, 187);
+            label6.Name = "label6";
+            label6.Size = new System.Drawing.Size(65, 13);
+            label6.TabIndex = 3;
+            label6.Text = "N (вариант)";
+            // 
+            // textBoxN
+            // 
+            this.textBoxN.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.errorProvider1.SetError(this.textBoxN, "Введите целое число");
+            this.textBoxN.Location = new System.Drawing.Point(101, 184);
+            this.textBoxN.Name = "textBoxN";
+            this.textBoxN.Size = new System.Drawing.Size(37, 20);
+            this.textBoxN.TabIndex = 2;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.BlinkRate = 120;
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(678, 470);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView dgv1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxN2;
+        private System.Windows.Forms.TextBox textBoxN1;
+        private System.Windows.Forms.TextBox textBoxC3;
+        private System.Windows.Forms.TextBox textBoxC2;
+        private System.Windows.Forms.TextBox textBoxC1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxN;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
     }
 }
 
--- МАПР/mapr3/mapr3/Form1.cs
+++ МАПР/mapr3/mapr3/Form1.cs
@@ -6,14 +6,86 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using DataProc;
 
 namespace mapr3
 {
     public partial class Form1 : Form
     {
+        int c1, c2, c3, n1, n2, N;
+        int[,] e;
+        float[,] p;
+        int[] q;
+
         public Form1()
         {
             InitializeComponent();
+
+            c1 = 300;
+            c2 = 620;
+            c3 = 200;
+            n1 = 5;
+            n2 = 5;
+            N = 15;
+
+            errorProvider1.Clear();
+
+            textBoxC1.Text = c1.ToString();
+            textBoxC2.Text = c2.ToString();
+            textBoxC3.Text = c3.ToString();
+            textBoxN1.Text = n1.ToString();
+            textBoxN2.Text = n2.ToString();
+            textBoxN.Text = N.ToString();
+
+            e = new int[10, 10];
+            p = new float[10, 10];
+            q = new int[10];
+        }
+
+        private void button1_Click(object sender, EventArgs ep)
+        {
+            dgv1.Rows.Clear();
+            dgv1.Columns.Clear();
+
+            for (int i = 0; i < 10; i++)
+                dgv1.Columns.Add((i + 1).ToString(), "F" + (i + 1).ToString());
+
+            dgv1.Rows.Add(10);
+            for (int i = 0; i < 10; i++)
+                dgv1.Rows[i].HeaderCell.Value = "E" + (i + 1).ToString();
+
+            dgv1.AutoResizeColumns();
+
+            errorProvider1.Clear();
+
+            ctrlToVal(textBoxC1, out c1);
+            ctrlToVal(textBoxC2, out c2);
+            ctrlToVal(textBoxC3, out c3);
+            ctrlToVal(textBoxN1, out n1);
+            ctrlToVal(textBoxN2, out n2);
+            ctrlToVal(textBoxN, out N);
+
+            for (int i = 0; i < 10; i++)
+                for (int j = 0; j < 10; j++)
+                {
+                    e[i, j] = 10 * i + j + 11;
+                }
+
+            MatrixToGrid(e, dgv1);
+        }
+
+        private void MatrixToGrid(int[,] a, DataGridView dgv)
+        {
+            // check rows columns
+            for (int i = 0; i < 10; i++)
+                for (int j = 0; j < 10; j++)
+                    dgv[j, i].Value = a[i, j];
+        }
+
+        private void ctrlToVal(TextBox textBox, out int val)
+        {
+            if (!int.TryParse(textBox.Text, out val))
+                errorProvider1.SetError(textBox, "Ожидается целое число");
         }
     }
 }
