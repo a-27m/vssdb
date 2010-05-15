e1f384a05ef41ca640d79bd89f272cad78144e46
Axell <Axell@bf672a44-3a04-1d4a-850b-c2a239875c8c>
2010-05-15 13:27:56 +0000

    git-svn-id: https://vaio/svn/vssdb@467 bf672a44-3a04-1d4a-850b-c2a239875c8c

---
+++ AI/Saiery/testds/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Microsoft.DirectX;
+
+
+namespace testds
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
