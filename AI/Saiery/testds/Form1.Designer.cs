6c8ef9e82807056905296539f30c7a2844fea327
Axell <Axell@bf672a44-3a04-1d4a-850b-c2a239875c8c>
2010-05-19 17:40:00 +0000

    git-svn-id: https://vaio/svn/vssdb@470 bf672a44-3a04-1d4a-850b-c2a239875c8c

--- AI/Saiery/testds/Form1.Designer.cs
+++ AI/Saiery/testds/Form1.Designer.cs
@@ -34,21 +34,21 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(136, 53);
+            this.button1.Location = new System.Drawing.Point(96, 57);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(95, 46);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Go";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(109, 122);
+            this.button2.Location = new System.Drawing.Point(96, 109);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(95, 46);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "Stop";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.buttonStop_Click);
             // 
--- AI/Saiery/testds/Form1.cs
+++ AI/Saiery/testds/Form1.cs
@@ -20,56 +20,69 @@ namespace testds
             InitializeComponent();
 
             dev = new Device();
+            dev.SetCooperativeLevel(this, CooperativeLevel.Normal);            
         }
 
         const int samplerate = 22050;
         const int bitspersample = 16;
 
-        MemoryStream pcm;
+        //MemoryStream pcm;
+        Int16[] pcm;
         Device dev;
         SecondaryBuffer buffer;
 
         private void button1_Click(object sender, EventArgs e)
         {
-            // Int16[] pcm = new Int16[samplerate * 60 * 3];
-            pcm = new MemoryStream(samplerate * 60 * 3 * 16 / 8);
+            pcm = new Int16[samplerate * 60 * 1]; // Int16[]
+            //pcm = new MemoryStream(samplerate * 60 * 3 * 16 / 8);
 
             for (int i = 0; i < pcm.Length / 2; i++)
             {
-                Int16 sample =(Int16)f(i);
-                pcm.WriteByte((byte)(sample >> 8));
-                pcm.WriteByte((byte)(sample & 255));
+                //Int16 sample =(Int16)f(i);
+                //pcm.WriteByte((byte)(sample >> 8));
+                //pcm.WriteByte((byte)(sample & 255));
+
+                pcm[i] = (Int16)f(i);
             }
 
             Stop();
 
-            WriteToFile("my.wav", pcm);
+            //WriteToFile("my.wav", pcm);
 
-            MessageBox.Show("Done!");
+            //MessageBox.Show("Done!");
 
-            Play("my.wav");
+            Play(pcm);
         }
 
         private void Stop()
         {
-            buffer.Stop();
+            if (buffer != null)
+                buffer.Stop();
         }
 
         private int f(float i)
         {
             double f = 0;
-            //f += Math.Sin(i * (2f * Math.PI) / samplerate * 1000f) * 16000f;
-            //f += Math.Sin(i * (2f * Math.PI) / samplerate * 100f ) * 16000f;
-            f += Math.Sin(
-                (2f * Math.PI) / samplerate * i* (
-                Math.Sin((2f * Math.PI)/samplerate*i*2)*20+500)// frequency
-                    //(Math.Sin(i / samplerate * (2f * Math.PI) * 0.5f) * 50f + 50f)
-               ) * 20000f;
-            /*f += Math.Sin(
-                (2f * Math.PI) / samplerate * i * 1000// frequency
-                //(Math.Sin(i / samplerate * (2f * Math.PI) * 0.5f) * 50f + 50f)
-               ) * 2000f;
-             */
+
+            Random rand = new Random();
+
+            f += Math.Sin(i * (2f * Math.PI) / samplerate * 100f) * 8000f;
+
+            f += Math.Sin(i * (2f * Math.PI) / samplerate * 50 ) * 1600f;
+
+            f += rand.NextDouble() * 16000 - 8000;
+
+            //f += Math.Sin(
+            //    (2f * Math.PI) / samplerate * i* (
+            //    Math.Sin((2f * Math.PI)/samplerate*i*2)*20+500)// frequency
+            //        //(Math.Sin(i / samplerate * (2f * Math.PI) * 0.5f) * 50f + 50f)
+            //   ) * 20000f;
+
+            //f += Math.Sin(
+            //    (2f * Math.PI) / samplerate * i * 1000// frequency
+            //    //(Math.Sin(i / samplerate * (2f * Math.PI) * 0.5f) * 50f + 50f)
+            //   ) * 2000f;
+
             return (int)f;
 
             //return (i-11025)*2*((i%2)*2-1);
@@ -112,7 +125,7 @@ namespace testds
             f.Write((Int16)2); // BlockAlign
             f.Write((Int16)16); // BitsPerSample
             f.Write("data".ToCharArray());
-            f.Write((Int32)words.Length * 2); // Length;
+            f.Write((Int32)words.Capacity * 2); // Length;
             
             words.WriteTo(f.BaseStream);
 
@@ -121,9 +134,52 @@ namespace testds
 
         private void Play(string filename)
         {
-            buffer = new SecondaryBuffer(filename, dev);
+            StreamReader reader = new StreamReader(filename);
 
-            buffer.Play(10, BufferPlayFlags.Default);
+            WaveFormat format = new WaveFormat();
+            format.AverageBytesPerSecond = bitspersample / 8 * samplerate;
+            format.BitsPerSample = bitspersample;
+            format.BlockAlign = bitspersample / 8;
+            format.Channels = 1;
+            format.FormatTag = WaveFormatTag.Pcm;
+            format.SamplesPerSecond = samplerate;
+
+            BufferDescription desc = new BufferDescription(format);
+            desc.BufferBytes = (int)reader.BaseStream.Length;
+            desc.ControlVolume = true;
+            desc.GlobalFocus = true;
+
+            buffer = new SecondaryBuffer(desc, dev);
+            //buffer = new SecondaryBuffer(filename, dev);
+            buffer.Write(0, reader.BaseStream, (int)reader.BaseStream.Length, LockFlag.None);
+
+            reader.Close();
+
+            //m_BufferBytes = buffer.Caps.BufferBytes;
+
+
+            buffer.Play(0, BufferPlayFlags.Default);
+        }
+
+        private void Play(Int16[] data)
+        {
+            WaveFormat format = new WaveFormat();
+            format.AverageBytesPerSecond = bitspersample / 8 * samplerate;
+            format.BitsPerSample = bitspersample;
+            format.BlockAlign = bitspersample / 8;
+            format.Channels = 1;
+            format.FormatTag = WaveFormatTag.Pcm;
+            format.SamplesPerSecond = samplerate;
+
+            BufferDescription desc = new BufferDescription(format);
+            desc.BufferBytes = (int)(bitspersample/8*data.Length);
+            desc.ControlVolume = true;
+            desc.GlobalFocus = true;
+
+            buffer = new SecondaryBuffer(desc, dev);
+            buffer.Write(0, data, LockFlag.None);
+
+            buffer.Play(0, BufferPlayFlags.Default);
         }
 
         private void buttonStop_Click(object sender, EventArgs e)
