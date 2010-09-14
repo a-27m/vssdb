96e55513c47f8d263a9f3c4de93c9ee957043c3b
Axell <Axell@bf672a44-3a04-1d4a-850b-c2a239875c8c>
2010-09-14 21:52:40 +0000

    git-svn-id: https://vaio/svn/vssdb@476 bf672a44-3a04-1d4a-850b-c2a239875c8c

---
+++ Crypt/lab1/lab1/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxMsg = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxMsg
+            // 
+            this.textBoxMsg.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxMsg.Location = new System.Drawing.Point(47, 7);
+            this.textBoxMsg.Name = "textBoxMsg";
+            this.textBoxMsg.Size = new System.Drawing.Size(293, 25);
+            this.textBoxMsg.TabIndex = 0;
+            this.textBoxMsg.Text = "This is some sample message.";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(12, 38);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(153, 244);
+            this.listBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(187, 120);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(220, 162);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Msg";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(265, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 292);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBoxMsg);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxMsg;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Crypt/lab1/lab1/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Forms;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+
+namespace lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            frq_dict = new Dictionary<char, float>();
+        }
+
+        Dictionary<Char, float> frq_dict;
+        int sort_direction = 1;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frq_dict.Clear();
+
+            textBoxMsg.Enabled = false;
+
+            for (int i = 0; i < textBoxMsg.TextLength; i++)
+            {
+                char key = textBoxMsg.Text[i];
+
+                if (!frq_dict.ContainsKey(key))
+                    frq_dict.Add(key, 1); // the first occurance
+                else
+                    frq_dict[key] = frq_dict[key] + 1;
+            }
+
+            textBoxMsg.Enabled = true;
+
+            List<KeyValuePair<char, float>> l = frq_dict.ToList();
+            l.Sort(
+                new Comparison<KeyValuePair<char, float>>(
+                    delegate(KeyValuePair<char, float> kv, KeyValuePair<char, float> kv2)
+                    {
+                        return (int)Math.Sign( sort_direction * (kv.Value - kv2.Value)); //kv.Key.CompareTo(kv2.Key);
+                    }
+                ));
+            listBox1.DataSource = l;
+
+            float txtLength = (float)textBoxMsg.TextLength; 
+            KeyValuePair<char, float>[] kvList = frq_dict.ToArray();
+            for (int i = 0; i < kvList.Length; i++)
+            {
+                kvList[i] = new KeyValuePair<char,float>(kvList[i].Key, kvList[i].Value / txtLength);
+            }
+
+
+        }
+    }
+
+    class Node
+    {
+        Node parent;
+
+    }
+}
