fb26383974088044642453fb95f10f2d975920a2
Axell <Axell@bf672a44-3a04-1d4a-850b-c2a239875c8c>
2010-09-15 22:07:26 +0000

    git-svn-id: https://vaio/svn/vssdb@479 bf672a44-3a04-1d4a-850b-c2a239875c8c

--- Crypt/lab1/lab1/Form1.Designer.cs
+++ Crypt/lab1/lab1/Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.textBoxMsg = new System.Windows.Forms.TextBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.dgv1 = new System.Windows.Forms.DataGridView();
@@ -37,32 +36,27 @@
             this.colProbability = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colCode = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBoxMsg
             // 
+            this.textBoxMsg.Dock = System.Windows.Forms.DockStyle.Top;
             this.textBoxMsg.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBoxMsg.Location = new System.Drawing.Point(47, 7);
+            this.textBoxMsg.Location = new System.Drawing.Point(43, 3);
             this.textBoxMsg.Name = "textBoxMsg";
-            this.textBoxMsg.Size = new System.Drawing.Size(293, 25);
+            this.textBoxMsg.Size = new System.Drawing.Size(393, 25);
             this.textBoxMsg.TabIndex = 0;
-            this.textBoxMsg.Text = "This is some sample message.";
-            // 
-            // listBox1
-            // 
-            this.listBox1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 15;
-            this.listBox1.Location = new System.Drawing.Point(87, 111);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(153, 64);
-            this.listBox1.TabIndex = 1;
+            this.textBoxMsg.Text = "this is some sample message.";
             // 
             // label1
             // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Location = new System.Drawing.Point(6, 8);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(28, 13);
             this.label1.TabIndex = 3;
@@ -70,11 +64,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(346, 7);
+            this.button1.Location = new System.Drawing.Point(442, 3);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 25);
+            this.button1.Size = new System.Drawing.Size(63, 24);
             this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
+            this.button1.Text = "Start 1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -85,9 +79,11 @@
             this.colChar,
             this.colProbability,
             this.colCode});
-            this.dgv1.Location = new System.Drawing.Point(12, 38);
+            this.tableLayoutPanel1.SetColumnSpan(this.dgv1, 4);
+            this.dgv1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgv1.Location = new System.Drawing.Point(3, 33);
             this.dgv1.Name = "dgv1";
-            this.dgv1.Size = new System.Drawing.Size(409, 242);
+            this.dgv1.Size = new System.Drawing.Size(569, 235);
             this.dgv1.TabIndex = 5;
             // 
             // colChar
@@ -114,40 +110,66 @@
             // 
             // tableLayoutPanel1
             // 
-            this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(468, 92);
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 69F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 67F));
+            this.tableLayoutPanel1.Controls.Add(this.dgv1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxMsg, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 3, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(200, 100);
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(575, 291);
             this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.label2, 4);
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Location = new System.Drawing.Point(3, 271);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(569, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Ready";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(511, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(54, 24);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Start 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(819, 292);
+            this.ClientSize = new System.Drawing.Size(575, 291);
             this.Controls.Add(this.tableLayoutPanel1);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.dgv1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBoxMsg);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Lab1 Huffman";
             ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.TextBox textBoxMsg;
-        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dgv1;
@@ -155,6 +177,8 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn colProbability;
         private System.Windows.Forms.DataGridViewTextBoxColumn colCode;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Crypt/lab1/lab1/Form1.cs
+++ Crypt/lab1/lab1/Form1.cs
@@ -21,25 +21,12 @@ namespace lab1
 
         private void button1_Click(object sender, EventArgs e)
         {
+            dgv1.Rows.Clear();
+
             tree = new Tree();
             Tree.Node[] leaves;
 
-            Dictionary<char, float> dict = new Dictionary<char, float>();
-
-            for (int i = 0; i < textBoxMsg.Text.Length; i++)
-            {
-                char c = textBoxMsg.Text[i];
-                if (dict.ContainsKey(c))
-                {
-                    dict[c] = dict[c] + 1;
-                }
-                else
-                {
-                    dict.Add(c, 1);
-                }
-            }
-
-            pairs = dict.ToArray();
+            BuildPairs();
 
             // less freaquent will be first ones
             Array.Sort(pairs, new Comparison<KeyValuePair<char, float>>(
@@ -57,7 +44,7 @@ namespace lab1
                 leaves[i] = new Tree.Node();
                 leaves[i].k = pairs[i].Key;
                 // TODO count total probability and let last leave's p to be = (1 - sum)
-                leaves[i].p = pairs[i].Value / (float)textBoxMsg.TextLength;
+                leaves[i].p = pairs[i].Value;// / (float)textBoxMsg.TextLength;
             }
 
             int index = 0;
@@ -83,6 +70,49 @@ namespace lab1
             }
 
             GatherCodes(leaves[pairs.Length - 1]);
+
+            double H = 0;
+
+            for (int k = 0; k < pairs.Length; k++)
+            {
+                H -= pairs[k].Value * Math.Log(pairs[k].Value, 2);
+            }
+
+            double C = 0;
+
+            for (int k = 0; k < pairs.Length; k++)
+            {
+                C += pairs[k].Value * dgv1[2, k].Value.ToString().Length;
+            }
+
+            double R = C - H;
+
+            label2.Text = string.Format("H = {0}, C = {1}, R = {2}", H, C, R);
+        }
+
+        private void BuildPairs()
+        {
+            Dictionary<char, float> dict = new Dictionary<char, float>();
+
+            for (int i = 0; i < textBoxMsg.Text.Length; i++)
+            {
+                char c = textBoxMsg.Text[i];
+                if (dict.ContainsKey(c))
+                {
+                    dict[c] = dict[c] + 1;
+                }
+                else
+                {
+                    dict.Add(c, 1);
+                }
+            }
+
+            pairs = dict.ToArray();
+
+            for (int i = 0; i < pairs.Length; i++)
+            {
+                pairs[i] = new KeyValuePair<char, float>(pairs[i].Key, pairs[i].Value / (float)textBoxMsg.TextLength);
+            }
         }
 
         private void PrintCodes()
@@ -92,12 +122,13 @@ namespace lab1
 
         object[] GatherCodes(Tree.Node root)
         {
+            indexDGV = 0;
             Gardener(root.children[0], "0");
             Gardener(root.children[1], "1");
 
             return null;
         }
-
+        int indexDGV = 0;
         void Gardener(Tree.Node node, string prefix)
         {
             if (node.children.Count > 0)
@@ -108,13 +139,87 @@ namespace lab1
             else
             {
                 // its leaf
-                listBox1.Items.Add("Node: "+node.k + " --> " + prefix);
+                dgv1.Rows.Add();
+                dgv1[0, indexDGV].Value = node.k;
+                dgv1[1, indexDGV].Value = node.p.ToString("F3");
+                dgv1[2, indexDGV].Value = prefix;
+                indexDGV++;
+                //listBox1.Items.Add("Node: "+node.k + " --> " + prefix);
             }
         }
 
         Tree.Node MakeNode(Tree.Node n1, Tree.Node n2)
         {
             return new Tree.Node('#', n1.p + n2.p);
+        }
+     
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dgv1.Rows.Clear();
+
+            BuildPairs();
+
+            // less freaquent will be first ones
+            Array.Sort(pairs, new Comparison<KeyValuePair<char, float>>(
+                delegate(KeyValuePair<char, float> a, KeyValuePair<char, float> b)
+                {
+                    return a.Value.CompareTo(b.Value);
+                }
+                )
+                );
+
+            indexDGV = 0;
+            int n = pairs.Length;
+            float[] B = new float[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                B[i] = 0;
+                for (int j = 0; j < i; j++)
+                {
+                    B[i] += pairs[j].Value;
+                }
+                B[i] += pairs[i].Value / 2.0f;
+
+                int m = (int)Math.Ceiling(Math.Log(1f / pairs[i].Value)) +1;
+
+                string code = "";
+                float b = B[i];
+                for (int k = 0; k <= m; k++)
+                { 
+                    b *= 2f;
+                    int integer = (int)Math.Truncate(b);                    
+                    code += integer.ToString();
+
+                    b -= integer;
+                }
+
+                dgv1.Rows.Add();
+                dgv1[0, indexDGV].Value = pairs[i].Key;
+                dgv1[1, indexDGV].Value = pairs[i].Value.ToString("F3");
+                dgv1[2, indexDGV].Value = code;
+                indexDGV++;
+            }
+
+            
+
+            double H = 0;
+
+            for (int k = 0; k < pairs.Length; k++)
+            {
+                H -= pairs[k].Value * Math.Log(pairs[k].Value, 2);
+            }
+
+            double C = 0;
+
+            for (int k = 0; k < pairs.Length; k++)
+            {
+                C += pairs[k].Value * dgv1[2, k].Value.ToString().Length;
+            }
+
+            double R = C - H;
+
+            label2.Text = string.Format("H = {0}, C = {1}, R = {2}", H, C, R);
         }
     }
 }
