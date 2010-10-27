8685ea346658007e649b3253a80db5f8088a607a
Axell <Axell@bf672a44-3a04-1d4a-850b-c2a239875c8c>
2010-10-27 19:59:38 +0000

    git-svn-id: https://vaio/svn/vssdb@511 bf672a44-3a04-1d4a-850b-c2a239875c8c

---
+++ Fulltext TF-IDF/TF-IDF/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace TF_IDF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBoxCount = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(420, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(26, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(172, 290);
+            this.listBox1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(205, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(209, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "C:\\Users\\sp10s-19\\Desktop\\war.txt";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Text (*.txt)|*.txt|All files (*.*)|*.*";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileName = "data.txt";
+            this.saveFileDialog1.Filter = "Text (*.txt)|*.txt|All files (*.*)|*.*";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(420, 52);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(26, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(205, 52);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(209, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "C:\\Users\\sp10s-19\\Desktop\\data.txt";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(277, 170);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 35);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBoxCount
+            // 
+            this.textBoxCount.Location = new System.Drawing.Point(277, 108);
+            this.textBoxCount.Name = "textBoxCount";
+            this.textBoxCount.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCount.TabIndex = 4;
+            this.textBoxCount.Text = "100";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(290, 241);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Ready";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 388);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxCount);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBoxCount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ Fulltext TF-IDF/TF-IDF/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+/*
+Реализовать пространственно-векторную модель поиска документов. Для этого:
+
+Документы индексировать ключевыми словами. Вес каждого слова определяется местом по убыванию важности ,например, по убыванию частот или по обращенному рангу. 
+Запросы задаются набором ключевых слов по убыванию важности. 
+Релевантность документа определяется скалярным произведением вектора запроса на вектор документа. 
+Основные соотношения: 
+1) инверсная частота термина: w(Ti)=lg(N/Ni), где Ti - термин, i=1,2,…M, M – число терминов в базе, N - число документов в базе, Ni - число документов с термином Ti в базе; 
+
+2) вес термина Ti в документе Dj: W=Wij= w(Ti)* wj(Ti), где wj(Ti) – частота термина Ti в документе Dj; 
+
+3) запрос формируется как вектор Q=(q1,q2, … ,qM), где qi– вес термина Ti в запросе; 
+
+4) степень релевантности документа Dj запросу Q определяется cкалярным произведением: Rj(Q)=QW=Σ(qi*Wij) в порядке убывания. 
+
+*/
+namespace TF_IDF
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, uint> dictBase;
+        Dictionary<string, uint> dictBoolCount;
+        List<Dictionary<string, uint>> dictDocs;
+
+        public Form1()
+        {
+            InitializeComponent();
+            dictBase = new Dictionary<string, uint>();
+            dictBoolCount = new Dictionary<string, uint>();
+            dictDocs = new List<Dictionary<string, uint>>();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox2.Text = saveFileDialog1.FileName;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Working...";
+            Application.DoEvents();
+            int minWordLength = int.Parse(textBoxCount.Text);
+
+            StreamReader finp = new StreamReader(textBox1.Text, Encoding.GetEncoding(1251));
+            //StreamWriter fout = new StreamWriter(textBox2.Text, false, Encoding.GetEncoding(1251));
+
+            char[] SentenceSepartors = new char[] { '.', '?', '!' };
+            char[] WordSeparators = new char[] { ' ', '\t', '.', '?', '!' };
+            Dictionary<string, uint> dictDoc = new Dictionary<string, uint>();
+
+            while (!finp.EndOfStream)
+            {
+                string line = finp.ReadLine();
+                Regex exp = new Regex("\\b\\w+\\b");
+                foreach(Match match in exp.Matches(line))
+                {
+                    string word = match.Value;
+                    word = word.ToLower();
+
+                    uint N;
+                    if (dictDoc.TryGetValue(word, out N))
+                        dictDoc[word] = N + 1;
+                    else
+                        dictDoc.Add(word, 1);
+
+                    //fout.WriteLine(SentNumber.ToString("D6") + "\t" + words[i]);
+                }
+            }
+
+            // merge                      
+
+            // update keyword apperances in documents
+            foreach (KeyValuePair<string, uint> word in dictDoc)
+            {
+                dictBoolCount[word.Key] = dictBoolCount[word.Key] + 1;
+            }
+
+
+            dictDocs.Add(dictDoc);
+
+            //Debug.Print(dictDoc.ToString());
+            //var res = dictBase.OrderByDescending(p => p.Value);
+
+            finp.Close();
+            //fout.Close();
+
+            label1.Text = "OK!";
+        }
+
+        object Query(KeyValuePair<string, uint>[] query)
+        { 
+            float[] r = new float[dictDocs.Count];
+            int i = 0;
+
+            foreach (Dictionary<string, uint> dictDoc in dictDocs)
+            {
+                foreach (KeyValuePair<string, uint> term in query)
+                    r[i++] = dictDoc[term.Key] * term.Value;
+            }
+
+            return r;
+        }
+    }
+}
