db631aa28cbfb6c3953981dc11b1fc34f336e326
Axell <Axell@bf672a44-3a04-1d4a-850b-c2a239875c8c>
2010-11-02 06:26:00 +0000

    git-svn-id: https://vaio/svn/vssdb@515 bf672a44-3a04-1d4a-850b-c2a239875c8c

--- KG/KG5 Triang/KG5 Triang/Form1.cs
+++ KG/KG5 Triang/KG5 Triang/Form1.cs
@@ -6,28 +6,57 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Collections;
 
 namespace KG5_Triang
 {
-    public partial class Form1 : Form
+    public partial class Form1 : Form, IComparer<PointF>
     {
         //int[] convex;
         PointF[] pts;
         Random rnd;
         Font font;
         Pen penConvex;
-        Pen penTriangle;
+        Pen penTriangle, penTriangleNF;
         //int gridStep = 50;
-        IList<Triangle> triangles;
+        IList<Triangle> triangles, trianglesNoFlip;
 
-        void Flip(ref Triangle A, ref Triangle B)
+        /// <summary>
+        /// Used for sorting in Compare() only!
+        /// </summary>
+        float x0, y0;
+
+        public static bool Flip(ref Triangle A, ref Triangle B)
         {
-            //int i1, j1, i2, j2;
+            int[] i = new int[2];
+            int[] j = new int[2];
+            int k=3, l=3;
+            int eq = 0;
 
-            //for(int i = 0; i < 3; i++)
-            //for(int j = 0; j < 3; j++)
-            //if (A.v[i] == B.v[j])
-            //{i1 = i;}
+            for (int p = 0; p < 3; p++)
+                for (int q = 0; q < 3; q++)
+                    if (A.v[p] == B.v[q])
+                    {
+                        if (eq > 2-1) return false;
+
+                        i[eq] = p;
+                        j[eq] = q;
+
+                        k -= p;
+                        l -= q;
+
+                        eq++;
+                    }
+            
+            // if (eq == 0) arn't adjacent;
+            if (eq != 2) return false;
+            // if (eq == 3) are the same one triangle;
+
+            //PointF tmp = A.v[i[1]];
+            A.v[i[1]] = B.v[l];
+            B.v[j[0]] = A.v[k];
+
+            return true;
         }
 
         IList<Triangle> SHull(PointF[] points)
@@ -36,26 +65,14 @@ namespace KG5_Triang
 
             // select a seed point x_0 from points[], 
             // sort according to |x_i - x_0|^2.
-            float x0, y0;
             x0 = points[0].X;
             y0 = points[0].Y;
 
-            Array.Sort<PointF>(points, 
-                delegate(PointF a, PointF b)
-                {
-                    float dx_ab, dy_ab;
-                    dx_ab = a.X - b.X;
-                    dy_ab = a.Y - b.Y;
-                    return Math.Sign(
-                        dx_ab * (a.X + b.X) - 2 * x0 * dx_ab +
-                        dy_ab * (a.Y + b.Y) - 2 * y0 * dy_ab
-                        );
-                }
-            );
+            Array.Sort<PointF>(points, this);
 
             List<int> convhull = new List<int>();
 
-            if (IsCCW(points[0], points[1], points[2]))
+            if (Triangle.MakesCCW(points[0], points[1], points[2]))
             {
                 // keep [0 1 2]
                 convhull.Add(0);
@@ -74,21 +91,10 @@ namespace KG5_Triang
                 trs.Add(new Triangle(points[0], points[2], points[1]));
             }
 
-            x0 = (points[0].X + points[1].X + points[2].X) / 3f;
-            y0 = (points[0].Y + points[1].Y + points[2].Y) / 3f;
+            x0 = trs[0].center.X;// (points[0].X + points[1].X + points[2].X) / 3f;
+            y0 = trs[0].center.Y;// (points[0].Y + points[1].Y + points[2].Y) / 3f;
 
-            Array.Sort<PointF>(points, 
-                delegate(PointF a, PointF b)
-                {
-                    float dx_ab, dy_ab;
-                    dx_ab = a.X - b.X;
-                    dy_ab = a.Y - b.Y;
-                    return Math.Sign(
-                        dx_ab * (a.X + b.X) - 2 * x0 * dx_ab +
-                        dy_ab * (a.Y + b.Y) - 2 * y0 * dy_ab
-                        );
-                }
-            );
+            Array.Sort<PointF>(points,3,points.Length-3, this);
 
             for (int i = 3; i < points.Length; i++)
             { 
@@ -97,17 +103,16 @@ namespace KG5_Triang
 
                 int startEdge = 0;
                 int endingEdge = 0;
-                int edgeIndex = 0;
                 bool visible;
 
-                foreach (int edge in convhull)
+                for (int edge = 0; edge < convhull.Count; edge++) 
                 {
                     int edgeNext = edge + 1;
                     edgeNext = edgeNext % convhull.Count;
 
-                    visible = TestVisible(points[i], points[edge], points[edgeNext]);
+                    visible = TestVisible(points[i], points[convhull[edge]], points[convhull[edgeNext]]);
 
-                    if (visible && edgeIndex == 0)
+                    if (visible && edge == 0)
                         prevVisible = true;
 
                     if (visible && !prevVisible)
@@ -129,6 +134,7 @@ namespace KG5_Triang
 
                     Triangle t;
                     t = new Triangle(points[convhull[edge]], points[i], points[convhull[edgeNext]]);
+                    t.MakeCCW();
                     trs.Add(t);
 
                     if (edge != startEdge && edge != endingEdge)
@@ -138,11 +144,40 @@ namespace KG5_Triang
                         //endingEdge--;
                         //if (endingEdge < 0) endingEdge += convhull.Count;
                     }
+
+                    if (edge == endingEdge) break;
                 }
 
-                foreach (int index in edgesToRemove)
-                    convhull.Remove(index);
-                convhull.Insert(startEdge + 1, i);
+                convhull.Insert(
+                    (startEdge + 1) % convhull.Count,
+                    i
+                    ); 
+                
+                foreach (int item in edgesToRemove)
+                    convhull.Remove(item);
+                
+
+            }
+
+            trianglesNoFlip = new List<Triangle>();
+
+            foreach (Triangle t in trs)
+                trianglesNoFlip.Add(new Triangle(t.v[0], t.v[1], t.v[2]));
+
+            for (int i = 0; i < trs.Count-1; i++)
+            {
+                for (int j = i+1; j < trs.Count; j++)
+                {
+                    Triangle A = trs[i];
+                    Triangle B = trs[j];
+
+                    // if (needed) 
+                    {
+                        Flip(ref A, ref B);
+                        trs[i] = A;
+                        trs[j] = B;
+                    }
+                }
             }
 
             return trs;
@@ -157,19 +192,7 @@ namespace KG5_Triang
             OB.X = B.X - lookFrom.X;
             OB.Y = B.Y - lookFrom.Y;
 
-            return OA.X * OB.Y - OA.Y * OB.X > 0;
-        }
-
-        private bool IsCCW(PointF A, PointF B, PointF C)
-        {
-            PointF AB = PointF.Empty;
-            PointF AC = PointF.Empty;
-            AB.X = B.X - A.X;
-            AB.Y = B.Y - A.Y;
-            AC.X = C.X - A.X;
-            AC.Y = C.Y - A.Y;
-
-            return AB.X * AC.Y - AB.Y * AC.X < 0;
+            return OA.X * OB.Y - OA.Y * OB.X < 0;
         }
 
         public Form1()
@@ -180,7 +203,8 @@ namespace KG5_Triang
 
             font = new Font("Arial", 10f);
             penConvex = new Pen(Color.DarkGray, 2f);
-            penTriangle = new Pen(Color.Green, 1f);
+            penTriangle = new Pen(Color.LightGreen, 1f);
+            penTriangleNF = new Pen(Color.LightBlue, 1f);
 
             Generate();
         }
@@ -211,6 +235,18 @@ namespace KG5_Triang
             //    }
             //    g.DrawLine(penConvex, prev, pts[convex[0]]);
             //}
+            if (trianglesNoFlip != null)
+            {
+                int i = 0;
+                foreach (Triangle t in trianglesNoFlip)
+                {
+                    g.DrawLine(penTriangleNF, t.v[0], t.v[1]);
+                    g.DrawLine(penTriangleNF, t.v[1], t.v[2]);
+                    g.DrawLine(penTriangleNF, t.v[2], t.v[0]);
+                    g.DrawString(i.ToString(), font, Brushes.Blue, t.center);
+                    i++;
+                }
+            }
 
             if (triangles != null)
             {
@@ -220,7 +256,7 @@ namespace KG5_Triang
                     g.DrawLine(penTriangle, t.v[0], t.v[1]);
                     g.DrawLine(penTriangle, t.v[1], t.v[2]);
                     g.DrawLine(penTriangle, t.v[2], t.v[0]);
-                    g.DrawString(i.ToString(), font, Brushes.Green, t.center);
+                    g.DrawString(i.ToString(), font, Brushes.LightGreen, t.center);
                     i++;
                 }
             }
@@ -255,5 +291,20 @@ namespace KG5_Triang
 
             pictureBox1.Refresh();
         }
+
+        #region IComparer<PointF> Members
+
+        public int Compare(PointF a, PointF b)
+        {
+            float dx_ab, dy_ab;
+            dx_ab = a.X - b.X;
+            dy_ab = a.Y - b.Y;
+            return Math.Sign(
+                dx_ab * (a.X + b.X) - 2 * x0 * dx_ab +
+                dy_ab * (a.Y + b.Y) - 2 * y0 * dy_ab
+                );
+        }
+
+        #endregion
     }
 }
--- KG/KG5 Triang/KG5 Triang/Triangle.cs
+++ KG/KG5 Triang/KG5 Triang/Triangle.cs
@@ -35,5 +35,56 @@ namespace KG5_Triang
             center.X = (vertex1.X + vertex2.X + vertex3.X) / 3f;
             center.Y = (vertex1.Y + vertex2.Y + vertex3.Y) / 3f;
         }
+
+        public void MakeCCW()
+        {
+            if (!this.IsCCW)
+            {
+                // make [0 2 1]
+                PointF tmp = v[1];
+                v[1] = v[2];
+                v[2] = tmp;
+            }
+        }
+
+        public static bool MakesCCW(PointF A, PointF B, PointF C)
+        {
+            //PointF AB = PointF.Empty;
+            //PointF AC = PointF.Empty;
+            //AB.X = B.X - A.X;
+            //AB.Y = B.Y - A.Y;
+            //AC.X = C.X - A.X;
+            //AC.Y = C.Y - A.Y;
+            //return AB.X * AC.Y - AB.Y * AC.X > 0;
+
+            //
+            //   | xa ya 1 | 
+            //   | xb yb 1 | > 0  => CCW
+            //   | xc yc 1 |
+            //
+            return B.X * C.Y - C.X * B.Y - A.X * C.Y + C.X * A.Y + A.X * B.Y - B.X * A.Y > 0;
+        }
+
+        public bool IsCCW
+        {
+            get
+            {
+                //PointF AB = PointF.Empty;
+                //PointF AC = PointF.Empty;
+                //AB.X = v[1].X - v[0].X;
+                //AB.Y = v[1].Y - v[0].Y;
+                //AC.X = v[2].X - v[0].X;
+                //AC.Y = v[2].Y - v[0].Y;
+
+                //return AB.X * AC.Y - AB.Y * AC.X > 0;
+
+                //
+                //   | xa ya 1 | 
+                //   | xb yb 1 | > 0  => CCW
+                //   | xc yc 1 |
+                //
+                return v[1].X * v[2].Y - v[2].X * v[1].Y - v[0].X * v[2].Y + v[2].X * v[0].Y + v[0].X * v[1].Y - v[1].X * v[0].Y > 0;
+            }
+        }
     }
 }
