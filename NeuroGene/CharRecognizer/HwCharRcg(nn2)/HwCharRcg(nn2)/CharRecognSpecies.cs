2a51975c9a75add53b6d81d157d208a0ff042b2c
Axell <Axell@bf672a44-3a04-1d4a-850b-c2a239875c8c>
2011-05-08 16:27:40 +0000

    git-svn-id: https://vaio/svn/vssdb@530 bf672a44-3a04-1d4a-850b-c2a239875c8c

--- NeuroGene/CharRecognizer/HwCharRcg(nn2)/HwCharRcg(nn2)/CharRecognSpecies.cs
+++ NeuroGene/CharRecognizer/HwCharRcg(nn2)/HwCharRcg(nn2)/CharRecognSpecies.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NeuroGenes.Genetic;
+using NeuroGenes;
 
 namespace NeuroGenes
 {
-    internal class CharRecognSpecies : BaseDoubleSpecies<CharRecognSpecies>, CharRecogizerNetwork
+    internal class CharRecognSpecies : BaseDoubleSpecies<CharRecognSpecies>, CharRecognizerNetwork
     {
 
         public CharRecognSpecies()
